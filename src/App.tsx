ab91b4fa814d37affc2e61a9a37b4693160167a3
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-16 20:27:51 +0200

    Update App.tsx #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { ThemeProvider } from "@mui/material";
-import { useState } from "react";
-import "./App.css";
-import { Router } from "./components/router/Router";
-import { themeCreator } from "./utils/theme-creator/themeCreator";
+import { ThemeProvider } from "@mui/material"
+import { useState } from "react"
+import "./App.css"
+import { Router } from "./components/router/Router"
+import { themeCreator } from "./utils/theme-creator/themeCreator"
 
 function App() {
-  const [isDarkModOn, setDarkModeOn] = useState<boolean>(false);
+  const [isDarkModOn, setDarkModeOn] = useState<boolean>(false)
   const theme = themeCreator(isDarkModOn)
   return (
     <ThemeProvider theme={theme}>
-      <Router isLoggedIn={true} />  
+      App
+      <Router isLoggedIn />
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
