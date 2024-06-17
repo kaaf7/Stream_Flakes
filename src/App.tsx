f890e08dd4605e0d73b9e1b9c112d64a848c03ea
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-18 00:13:56 +0200

    Add Theme to App.tsx #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { ThemeProvider } from "@mui/material"
+import { CssBaseline, ThemeProvider } from "@mui/material"
 import { useState } from "react"
 import "./App.css"
 import { Router } from "./components/router/Router"
-import { themeCreator } from "./utils/theme-creator/themeCreator"
+import { defineTheme } from "./utils/theme-creator/themeCreator"
 
 function App() {
-  const [isDarkModOn, setDarkModeOn] = useState<boolean>(false)
-  const theme = themeCreator(isDarkModOn)
+  const [isDarkModOn, setDarkModeOn] = useState<boolean>(true)
+  const theme = defineTheme(isDarkModOn)
   return (
     <ThemeProvider theme={theme}>
-      App
+      <CssBaseline />
       <Router isLoggedIn />
     </ThemeProvider>
   )
