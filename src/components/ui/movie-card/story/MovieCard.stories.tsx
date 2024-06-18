14f61c86cb51867c5130e9a80b0333ecb0df7839
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-19 00:00:17 +0200

    Move MovieCard stories

---
+++ src/components/ui/movie-card/story/MovieCard.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, Story } from "@storybook/react"
+import { MovieCard, MovieCardProps } from "../MovieCard"
+
+const meta: Meta<typeof MovieCard> = {
+  title: "example/components/custom/MovieCard",
+  component: MovieCard,
+  parameters: {
+    layout: "fullscreen"
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh"
+        }}>
+        <Story />
+      </div>
+    )
+  ]
+}
+
+export default meta
+
+const Template: Story<MovieCardProps> = (args) => <MovieCard {...args} />
+
+export const Default = Template
+Default.args = {}
