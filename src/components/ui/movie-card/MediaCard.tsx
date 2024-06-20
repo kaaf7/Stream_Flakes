548121280714e579c0b2b8835dbe5d47b9ec1a04
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-21 00:06:19 +0200

    Update MediaCard and add Grid wrapper as item #5

--- src/components/ui/movie-card/MediaCard.tsx
+++ src/components/ui/movie-card/MediaCard.tsx
@@ -1,46 +1,43 @@
+import { Media } from "@/interfaces/Media"
 import { Favorite } from "@mui/icons-material"
-import { Box, Button, CardActionArea, CardMedia } from "@mui/material"
+import { Box, Button, CardActionArea, CardMedia, Grid } from "@mui/material"
 import Card from "@mui/material/Card"
 import CardActions from "@mui/material/CardActions"
 
-interface MediaCardProps {
-  title: string
-  imageUrl: string
-}
-
-export const MediaCard = ({ title, imageUrl }: MediaCardProps) => {
+export const MediaCard = ({ title, imageUrl }: Media) => {
   return (
-    <Card sx={{ width: 250, height: 250 }}>
-      <Box sx={{ position: "relative", height: "100%" }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            loading="lazy"
-            src={imageUrl}
-            image={imageUrl}
-            alt={title}
-            onError={(e) => {
-              e.currentTarget.src = ""
-            }}
-          />
-        </CardActionArea>
-        <CardActions
-          sx={{
-            display: "flex",
-            position: "absolute",
-            width: "100%",
-            justifyContent: "space-between",
-            bottom: 0,
-            left: 0,
-            color: "white",
-            backgroundColor: "rgba(0, 0, 0, 0.2)"
-          }}>
-          <Button size="small" color="inherit" variant="text">
-            Share
-          </Button>
-          <Favorite />
-        </CardActions>
-      </Box>
-    </Card>
+    <Grid item>
+      <Card sx={{ width: 250, height: 250 }}>
+        <Box sx={{ position: "relative", height: "100%" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              loading="lazy"
+              image={imageUrl}
+              alt={title}
+              onError={(e) => {
+                e.currentTarget.src = ""
+              }}
+            />
+          </CardActionArea>
+          <CardActions
+            sx={{
+              display: "flex",
+              position: "absolute",
+              width: "100%",
+              justifyContent: "space-between",
+              bottom: 0,
+              left: 0,
+              color: "white",
+              backgroundColor: "rgba(0, 0, 0, 0.2)"
+            }}>
+            <Button size="small" color="inherit" variant="text">
+              Share
+            </Button>
+            <Favorite />
+          </CardActions>
+        </Box>
+      </Card>
+    </Grid>
   )
 }
