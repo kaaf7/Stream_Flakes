a75136eb23a3fbe6a821fc95f5293e02d8d2f4b6
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-21 00:07:12 +0200

    Add Grid as container and map medias into MediaCard #5

--- src/features/media-grid/components/MediaGrid.tsx
+++ src/features/media-grid/components/MediaGrid.tsx
@@ -1,6 +1,13 @@
+import { MediaCard } from "@/components/ui/movie-card"
+import { Media } from "@/interfaces/Media"
+import { Grid } from "@mui/material"
 
-export const MediaGrid = () => {
+export const MediaGrid = (medias: Media[]) => {
   return (
-    <div></div>
+    <Grid container spacing={2}>
+      {medias.map((media: Media) => (
+        <MediaCard key={media.id} title={media.title} id={media.id} imageUrl={media.imageUrl} />
+      ))}
+    </Grid>
   )
 }
