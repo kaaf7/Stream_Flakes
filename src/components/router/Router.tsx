626892bfbb24de31ca8989b3d8316d7b79393961
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-23 19:06:55 +0200

    Update Router with new un/protectedRoutes  #5

--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -1,25 +1,36 @@
-import { LOGIN_PATH, MAIN_PATH } from "@/constants/constants"
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { LOGIN_PATH, MAIN_PATH, MEDIAS_PATH, USER_PATH } from "@/constants/constants"
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom"
+
+import { lazy } from "react"
 import { MainLayout } from "../ui/main-layout"
+
+const MediasOverview = lazy(() => import("../../pages/medias/MediasOverview"))
+
+const protectedRoutes = [
+  {
+    path: USER_PATH,
+    element: <></>
+  }
+]
 
 const routes = (isLoggedIn: boolean) => [
   {
     path: "*",
+    element: <Navigate to={MAIN_PATH} />,
+        
+  },
+  {
+    path: MEDIAS_PATH,
+    element: <MediasOverview />
+  },
+  {
+    path: LOGIN_PATH,
     element: <></>
   },
   {
     path: MAIN_PATH,
     element: <MainLayout isLoggedIn={isLoggedIn} />,
-    children: [
-      {
-        path: "",
-        element: <></>
-      }
-    ]
-  },
-  {
-    path: LOGIN_PATH,
-    element: <></>
+    children: protectedRoutes
   }
 ]
 
@@ -27,7 +38,6 @@ interface RouterProps {
   isLoggedIn: boolean
 }
 export const Router = ({ isLoggedIn }: RouterProps) => {
-  const loggedInRoutes = createBrowserRouter(routes(isLoggedIn))
-
-  return <RouterProvider router={loggedInRoutes} />
+  const  router= createBrowserRouter(routes(isLoggedIn))
+  return <RouterProvider router={router} />
 }
