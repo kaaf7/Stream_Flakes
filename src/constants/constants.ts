59851723b3edb845c97e2c4d731deaa41ebfa749
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-23 19:09:38 +0200

    Add new paths USER_PATH, SHOWS_PATH, FAVORITES_PATH , & SHARED_FAVORITES_PATH #5

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,15 @@
 export const MAIN_PATH = "/"
 
+export const USER_PATH = "/user"
+
 export const LOGIN_PATH = "/login"
 
 export const MEDIAS_PATH = "/medias"
+
 export const SHOWS_PATH = "/shows"
+
+export const FAVORITES_PATH = "/favorites"
+
+export const SHARED_FAVORITES_PATH = "/favorites/shared"
+
+
