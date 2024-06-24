ec9f0bb4b289e5388770865fe8c7e1740a81b018
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-24 21:49:51 +0200

    Create Fake Media #5

--- src/pages/medias/MediasOverview.tsx
+++ src/pages/medias/MediasOverview.tsx
@@ -1,4 +1,58 @@
+import { MediaGrid } from "@/features/media-grid"
+export default function MediasOverview() {
+  const medias = [
+    {
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    }, {
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    },{
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    },
+    {
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    }, {
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    },{
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    },
+    {
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    }, {
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    },{
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    },{
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    }, {
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    },{
+      title: "",
+      imageUrl:"https://i.ebayimg.com/images/g/UEkAAOSwjdliRZJx/s-l1600.jpg",
+      id: "https://static.wikia.nocookie.net/ironman/images/d/da/P170620_v_v8_ba.jpg/revision/latest?cb=20191202183622"
+    }
+  ]
 
-export default function MovieGrid() {
-  return <>medias</>
+  return <MediaGrid  medias ={medias}/>
 }
