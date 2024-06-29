b4933b026cd6ff005d5b6e9f6e5b502aec2ab1f9
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-29 22:50:35 +0200

    Import SearchResultsDisplay add new states and pass props to it #6

--- src/components/ui/search-bar/SearchBar.tsx
+++ src/components/ui/search-bar/SearchBar.tsx
@@ -1,25 +1,43 @@
 import { Container, TextField } from "@mui/material"
+import { ChangeEvent, useState } from "react"
+
+import { SearchResultsDisplay } from "@/components/ui/search-results-display"
 
 export const SearchBar = () => {
+  const [search, setSearch] = useState<string | null>(null)
+
+  const [resultBoxVisible, setResultBoxVisible] = useState<boolean>(false)
+
+  const handleFocus = () => {
+    setResultBoxVisible(true)
+  }
+  const handleBlur = () => {
+    setTimeout(() => setResultBoxVisible(false), 300)
+  }
+
   return (
-    <Container>
+    <Container sx={{ position: "relative" }}>
       <TextField
         id="search-bar"
+        name="search"
+        variant="outlined"
+        onChange={(event: ChangeEvent<HTMLInputElement>) => setSearch(event.target.value)}
+        value={search}
         placeholder="Search Films"
-        variant="standard"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        autoComplete="off"
         sx={{
-          width: "30rem",
+          width: "750px",
           background: "inherit",
           transition: "width 0.3s ease-in-out",
-          "&:focus-within": {
-            width: "45rem"
-          },
+          "&:focus-within": {},
           "& .MuiInput-underline:before": {
-            borderBottom: ".01rem solid", 
+            borderBottom: ".01rem solid",
             transition: "border-bottom-width 0.2s"
           },
           "&:hover .MuiInput-underline:before": {
-            borderBottom: ".01rem solid" 
+            borderBottom: ".01rem solid"
           },
           "& .MuiInput-underline:after": {
             borderBottom: ".01rem solid"
@@ -27,14 +45,13 @@ export const SearchBar = () => {
           "& .MuiOutlinedInput-root": {
             "&:hover .MuiOutlinedInput-notchedOutline": {
               borderColor: "none",
-              borderWidth: "0"
+              borderWidth: ".1px"
             },
             "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
               borderColor: "none",
-              borderWidth: "0"
+              borderWidth: ".1px"
             },
-            borderRadius: ".5rem",
-            height: "2rem"
+            height: "30px"
           },
           "& .MuiInputLabel-root": {
             transform: "translate(14px, .3rem) scale(1)",
@@ -46,6 +63,7 @@ export const SearchBar = () => {
           }
         }}
       />
+      <SearchResultsDisplay resultBoxVisible={resultBoxVisible} />
     </Container>
   )
 }
