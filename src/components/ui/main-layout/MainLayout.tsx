ebef0de0249a7e0fcbdfd8ce451a5921e2b730bf
Karim <108280260+kaaf7@users.noreply.github.com>
2024-06-30 16:45:45 +0200

    Update MainLayout and remove old unneeded components #6

--- src/components/ui/main-layout/MainLayout.tsx
+++ src/components/ui/main-layout/MainLayout.tsx
@@ -1,51 +1,20 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom"
 
-import movies from "@/assets/images/movies.jpg";
-import { NavBar } from "@/components/ui/nav-bar";
-import { LOGIN_PATH } from "@/constants/constants";
+import { NavBar } from "@/components/ui/nav-bar"
+import { LOGIN_PATH } from "@/constants/constants"
 
 interface MainLayoutProps {
-  isLoggedIn: boolean;
+  isLoggedIn: boolean
 }
 
 export function MainLayout({ isLoggedIn }: MainLayoutProps) {
   return isLoggedIn ? (
     <>
       <NavBar isLoggedIn />
-      <div 
-        style={{ 
-          position: "relative", 
-          width: "100%", 
-          height: "100%" 
-        }}
-      >
-        <img
-          src={movies}
-          alt="Movies"
-          loading="lazy"
-          style={{ 
-            width: "100%", 
-            height: "100%", 
-            objectFit: "cover",
-            filter: "blur(8px) brightness(40%)",
-          }}
-        />
-        <div 
-          style={{ 
-            position: "absolute", 
-            top: 0, 
-            left: 0, 
-            width: "100%", 
-            height: "100%", 
-            backgroundImage: "linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.2))",
-            zIndex: 1000
-          }}
-        />
-      </div>
-      <div style={{ background: "black", width: "100%", height: "100vh" }}></div>
       <Outlet />
     </>
   ) : (
     <Navigate to={LOGIN_PATH} />
-  );
+  )
 }
+
