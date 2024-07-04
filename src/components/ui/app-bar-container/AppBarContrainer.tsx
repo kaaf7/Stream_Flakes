537a73a21b2e7f26e2192b13ef40facf802f3933
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-04 22:56:43 +0200

    Update AppBarContainer to change its color on scroll #7

--- src/components/ui/app-bar-container/AppBarContrainer.tsx
+++ src/components/ui/app-bar-container/AppBarContrainer.tsx
@@ -1,6 +1,22 @@
-import { AppBar, AppBarProps, Toolbar } from "@mui/material"
+import { AppBar, AppBarProps, Toolbar, useTheme } from "@mui/material"
+import { useEffect, useState } from "react"
 
 export const AppBarContainer = ({ children }: AppBarProps) => {
+  const [scrolled, setScrolled] = useState(false)
+  const theme = useTheme()
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 200)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
     <AppBar
       sx={{
@@ -10,7 +26,10 @@ export const AppBarContainer = ({ children }: AppBarProps) => {
         alignItems: "center",
         position: "fixed",
         boxShadow: 0,
-        width: "100%"
+        width: "100%",
+        height: 70,
+        backgroundColor: scrolled ? theme.palette.background.default : "transparent",
+        transition: "background-color 0.1s ease-in-out"
       }}>
       <Toolbar
         sx={{
@@ -18,9 +37,9 @@ export const AppBarContainer = ({ children }: AppBarProps) => {
           display: "flex",
           direction: "column",
           position: "fixed",
-          alignContent:"center",
-          alignItems:"center",
-          gap: 1,
+          alignContent: "center",
+          alignItems: "center",
+          gap: 3,
           margin: 0
         }}>
         {children}
