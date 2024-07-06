85d82d9f8cfec36a375135147f711b493bf19bcf
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-06 14:04:42 +0200

    Create  icons.ts to export icons from #7

---
+++ src/features/home/media/icons.ts
@@ -0,0 +1,10 @@
+import icon_1 from "@/assets/icons/icon_1.svg"
+import icon_2 from "@/assets/icons/icon_2.svg"
+import icon_3 from "@/assets/icons/icon_3.svg"
+import icon_4 from "@/assets/icons/icon_4.svg"
+import icon_5 from "@/assets/icons/icon_5.svg"
+import icon_6 from "@/assets/icons/icon_6.svg"
+import icon_7 from "@/assets/icons/icon_7.svg"
+import icon_8 from "@/assets/icons/icon_8.svg"
+
+export const BRAND_ICONS = [icon_2, icon_8, icon_3, icon_4, icon_5, icon_6, icon_7 , icon_1]
