e7aa8633b5e63c8fa5f6d8e047d1f94e8554802b
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-08 23:58:30 +0200

    Export components and interfaces from index in account feature #7

--- src/features/account/index.ts
+++ src/features/account/index.ts
@@ -1,2 +1,8 @@
 export * from "./log-in/forms/LoginForm"
+
+export * from "./log-in/interfaces/LoginFormProps"
+export * from "./log-in/interfaces/LoginFormValues"
+
 export * from "./sign-up/forms/SignUpForm"
+export * from "./sign-up/interfaces/SignUpFormValues"
+
