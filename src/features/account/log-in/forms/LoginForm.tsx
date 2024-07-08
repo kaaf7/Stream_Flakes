9513d348274dd03ccaa911f8d8f8fa6569432664
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-08 20:21:01 +0200

    Create Basic LoginForm #7

--- src/features/account/log-in/forms/LoginForm.tsx
+++ src/features/account/log-in/forms/LoginForm.tsx
@@ -1,6 +1,27 @@
+import { Box, TextField } from "@mui/material"
 
 export const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <main
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center"
+      }}>
+      <Box>
+        <TextField
+          id="username"
+          name="username"
+          type="text"
+          aria-label="username"
+          aria-required="true"
+          variant="outlined"
+        />
+        <TextField id="password" name="password" type="password" />
+      </Box>
+    </main>
   )
 }
