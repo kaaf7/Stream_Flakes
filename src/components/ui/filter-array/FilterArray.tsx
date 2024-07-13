1f675afc93dfbbc14ff4834bef90513f34171f64
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-13 20:15:24 +0200

    Rename FilterList to FilterArray #7

---
+++ src/components/ui/filter-array/FilterArray.tsx
@@ -0,0 +1,46 @@
+import { Box } from "@mui/material"
+import Chip from "@mui/material/Chip"
+import { SyntheticEvent } from "react"
+
+interface FilterListProps {
+  filter: object
+  onFilterDelete(key: string): void
+  onFilterDeleteAll(event: SyntheticEvent): void
+}
+
+export const FilterArray = ({
+  filter,
+  onFilterDelete,
+  onFilterDeleteAll,
+}: FilterListProps) => {
+  const validFilterEntries = Object.entries(filter).filter(([k, v]) =>
+    ["string", "number", "boolean"].includes(typeof v)
+  )
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        position: "relative",
+        justifyContent: "flex-start",
+        gap: 1
+      }}>
+      {validFilterEntries.map(([k, v]) => (
+        <Chip
+          variant="outlined"
+          size="small"
+          label={`${k}: ${v}`}
+          onDelete={() => onFilterDelete(k)}
+        />
+      ))}
+      {validFilterEntries.length > 0 ? (
+        <Chip
+          variant="outlined"
+          size="small"
+          label={"Delete All Filters"}
+          onDelete={onFilterDeleteAll}
+        />
+      ) : null}
+    </Box>
+  )
+}
