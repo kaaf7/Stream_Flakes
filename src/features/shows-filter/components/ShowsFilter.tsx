4ba6bd3c3c07fa9c746264219e27494bfb947cbe
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-15 21:21:53 +0200

    Create ShowsFilter #7

--- src/features/shows-filter/components/ShowsFilter.tsx
+++ src/features/shows-filter/components/ShowsFilter.tsx
@@ -1,78 +1,127 @@
-import React, { useState } from "react"
-import {
-  SHOWS_FILTER_BY_GENRE,
-  SHOWS_FILTER_BY_NAME,
-  SHOWS_FILTER_BY_YEAR
-} from "@/constants/constants"
+import { Container, Drawer } from "@mui/material"
+import { ChangeEvent, Dispatch, SetStateAction, useState } from "react"
 import { URLSearchParamsInit, useSearchParams } from "react-router-dom"
 
-import { Drawer } from "@mui/material"
+import { CustomButton } from "@/components/buttons/custom-button"
+import { SHOWS_FILTER_BY_NAME } from "@/constants/constants"
+import { ShowsFilterInterface } from "@/features/shows-filter"
+import { createFormFields } from "@/utils/form-creator/createFormFields"
+import { useTranslation } from "react-i18next"
 
-interface ShowsFilterState {
-  [key: string]: string | number | null
+interface ShowsFilterProps {
+  setFilterOpen: Dispatch<SetStateAction<boolean>>
+  isFilterOPen: boolean
 }
 
-export const ShowsFilter = () => {
+export const ShowsFilter = ({ setFilterOpen, isFilterOPen }: ShowsFilterProps) => {
   const [searchParams, setSearchParams] = useSearchParams()
 
-  const [isFilterOpen, setFilterOpen] = useState<boolean>(false)
-
-  const [filter, setFilter] = useState<ShowsFilterState>({
-    name: searchParams.get(SHOWS_FILTER_BY_NAME),
-    genre: searchParams.get(SHOWS_FILTER_BY_GENRE),
-    year: searchParams.get(SHOWS_FILTER_BY_YEAR)
+  const [filter, setFilter] = useState<ShowsFilterInterface>({
+    name: searchParams.get(SHOWS_FILTER_BY_NAME)
   })
 
-  const updateFilter = (filter: ShowsFilterState) => {
-    const filterEmptyValues = Object.fromEntries(
-      Object.entries(filter).filter(([_, v]) => v !== null && v !== "" && v !== undefined)
-    )
-    setFilter(filterEmptyValues)
-    setSearchParams(filterEmptyValues as URLSearchParamsInit)
-  }
-
-  const onFilterDelete = (key: string) => {
-    const updatedFilter = Object.fromEntries(Object.entries(filter).filter(([k]) => k !== key))
-    updateFilter(updatedFilter)
-  }
+  const [filterState, setFilterState] = useState<ShowsFilterInterface>(filter)
 
   const onFilterDeleteAll = () => {
     updateFilter({})
   }
 
-  const toggleDrawer = (newOpen: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-    if (
-      event &&
-      event.type === "keydown" &&
-      ((event as React.KeyboardEvent).key === "Tab" ||
-        (event as React.KeyboardEvent).key === "Shift")
-    ) {
-      return
-    }
-    setFilterOpen(newOpen)
+  const { t } = useTranslation(["common"])
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setFilterState({ ...filterState, [name]: value })
   }
 
-  const FilterDrawer = () => (
-    <Drawer
-      sx={{
-        width: "400px",
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: "400px"
-        }
-      }}
-      anchor="right"
-      open={isFilterOpen}
-      onClose={toggleDrawer(false)}
-      //onOpen={toggleDrawer(true)}
-    ></Drawer>
-  )
+  const updateFilter = (formValues: ShowsFilterInterface) => {
+    const filterEmptyValues = Object.fromEntries(
+      Object.entries(formValues).filter(([_, v]) => v !== null && v !== "" && v !== undefined)
+    )
+    setFilter(filterEmptyValues)
+    setSearchParams(filterEmptyValues as URLSearchParamsInit)
+    setFilterState(filterEmptyValues)
+  }
+
+  const onSubmit = (filter: ShowsFilterInterface) => {
+    updateFilter(filter)
+    setFilterOpen((prevState) => !prevState)
+  }
+
+
+
+  const onFilterDelete = (key: string) => {
+    const updaetdFilter = Object.fromEntries(Object.entries(Object).filter(([_, v]) => k !== key))
+    updateFilter(updaetdFilter)
+  }
+
+  const FORM_FIELDS = [
+    {
+      id: t("main.filter.name"),
+      name: "name",
+      value: filterState.name ?? "",
+      label: t("main.filter.name"),
+      placeholder: t("main.filter.name"),
+      ariaPlaceHolder: t("main.filter.name"),
+      onChange
+    },
+    {
+      id: t("main.filter.genre"),
+      name: "genre",
+      value: filterState.genre ?? "",
+      label: t("main.filter.genre"),
+      placeholder: t("main.filter.genre"),
+      ariaPlaceHolder: t("main.filter.genre"),
+      onChange
+    },
+    {
+      id: t("main.filter.year"),
+      name: "year",
+      value: filterState.year ?? "",
+      label: t("main.filter.year"),
+      placeholder: t("main.filter.year"),
+      ariaPlaceHolder: t("main.filter.year"),
+      onChange
+    }
+  ]
+
+  const FilterFormFileds = createFormFields(FORM_FIELDS)
 
   return {
-    filter,
-    FilterDrawer,
-    toggleDrawer,
-    onFilterDelete,
-    onFilterDeleteAll
+    FilterDrawer: (
+      <Drawer
+        sx={{
+          width: "40rem",
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: "400px"
+          }
+        }}
+        anchor="right"
+        open={isFilterOPen}
+        onClose={() => setFilterOpen(false)}>
+        <Container
+          sx={{
+            height: "100%",
+            padding: "1rem 1rem 1rem 1rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            gap: 5
+          }}>
+          {FilterFormFileds}
+          <CustomButton
+            variant="outlined"
+            onClick={(e) => onSubmit(filterState)}
+            sx={{ width: "100%", height: "3rem" }}>
+            submit
+          </CustomButton>
+        </Container>
+      </Drawer>
+    ),
+    filter: filter,
+    onFilterDelete: onFilterDelete,
+    onFilterDeleteAll: onFilterDeleteAll,
+    onSubmit: onSubmit
   }
 }
