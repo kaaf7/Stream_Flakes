f0c4f01e50d6a73764c1a460f39416009a4139e0
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-15 21:21:26 +0200

    Import i18next for translations #7

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,33 @@ import "./App.css"
 
 import { CssBaseline, ThemeProvider } from "@mui/material"
 
+import { Router } from "@/components/router/Router"
+import englishTranslation from "@/translations/en/english.json"
+import { defineTheme } from "@/utils/theme-creator/themeCreator"
+import i18next from "i18next"
 import { useState } from "react"
-import { Router } from "./components/router/Router"
-import { defineTheme } from "./utils/theme-creator/themeCreator"
+import { I18nextProvider } from "react-i18next"
 
 function App() {
   const [isDarkModOn, setDarkModeOn] = useState<boolean>(true)
   const theme = defineTheme(isDarkModOn)
+
+  i18next.init({
+    interpolation: { escapeValue: false },
+    lng: "en",
+    resources: {
+      en: {
+        common: englishTranslation
+      }
+    }
+  })
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router isLoggedIn />
-    </ThemeProvider>
+    <I18nextProvider i18n={i18next}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router isLoggedIn />
+      </ThemeProvider>
+    </I18nextProvider>
   )
 }
 
