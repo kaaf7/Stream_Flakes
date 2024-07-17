c20ddf8b5443c5e7e6241e903fa30c358af3c36e
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-17 21:15:59 +0200

    Create SkeletonGrid #7

---
+++ src/components/ui/skeleton-grid/SkeletonGrid.tsx
@@ -0,0 +1,36 @@
+import { CardSkeleton, CardSkeletonProps } from "../card-skeleton/"
+
+import { ImageList } from "@mui/material"
+
+interface SkeletonGridProps {
+  minmax: string
+  gridWidth?: string
+  gridHeight?:string
+  gridLength: number
+  gap?: number
+  cardSkeletonProp?: CardSkeletonProps
+}
+
+export const SkeletonGrid = ({
+  gridWidth,
+  gridHeight,
+  gridLength,
+  cardSkeletonProp,
+  minmax,
+  gap
+}: SkeletonGridProps) => {
+  const cardSkeletons = Array.from({ length: gridLength }, (_, index) => (
+    <CardSkeleton {...cardSkeletonProp} key={index} />
+  ))
+  return (
+    <ImageList
+      gap={gap ?? 20}
+      sx={{
+        gridTemplateColumns: `repeat(auto-fill, minmax(${minmax}, 1fr)) !important`,
+        width: gridWidth ?? "100%",
+        height: gridHeight ?? "100%"
+      }}>
+      {cardSkeletons}
+    </ImageList>
+  )
+}
