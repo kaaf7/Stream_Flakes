06c3bcb418c981ec22f1c4c6f5f959ad29e60e75
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-17 21:08:27 +0200

    Add new Items ro RighNavbarItems and isProtected to  type Item #7

--- src/components/ui/right-navbar-items/RightNavBarItems.tsx
+++ src/components/ui/right-navbar-items/RightNavBarItems.tsx
@@ -8,13 +8,21 @@ type Item = {
   variant: "text" | "outlined" | "contained"
   size: "small" | "medium" | "large"
   to: string
+  isProtected?: boolean
 }
 
 const RIGHT_SIDE_ITEMS: Item[] = [
-  { title: "Signup", variant: "outlined", size: "small", to: SIGN_UP_PATH },
-  { title: "Login", variant: "text", size: "small", to: LOGIN_PATH },
-  { title: "Shows", variant: "text", size: "small", to: SHOWS_PATH },
-  { title: "Sports", variant: "text", size: "small", to: SPORTS_PATH }
+
+  { title: "Shows", variant: "text", size: "small", to: SHOWS_PATH, isProtected: false },
+  { title: "Sports", variant: "text", size: "small", to: SPORTS_PATH, isProtected: false }
+]
+
+const USER_RIGHT_SIDE_ITEMS: Item[] = [
+  { title: "Signup", variant: "outlined", size: "small", to: SIGN_UP_PATH, isProtected: true },
+  { title: "Login", variant: "outlined", size: "small", to: LOGIN_PATH, isProtected: true },
+  { title: "Account",variant: "text", size: "small", to: LOGIN_PATH, isProtected: false },
+  { title: "Logout", variant: "outlined", size: "small", to: LOGIN_PATH, isProtected: false },
+
 ]
 
 interface RightNavBarItemsProps extends ContainerProps {
@@ -36,6 +44,12 @@ export const RightNavBarItems = ({ isLoggedIn }: RightNavBarItemsProps) => {
         <CustomButton sx={{ height: "2rem" }} variant={item.variant} size={item.size} to={item.to}>
           {item.title}
         </CustomButton>
+      ))}
+     {USER_RIGHT_SIDE_ITEMS.filter((item) => item.isProtected !== isLoggedIn).map((item) => (
+        <CustomButton sx={{ height: "2rem" }} variant={item.variant} size={item.size} to={item.to}>
+          {item.title}
+        </CustomButton>
+        
       ))}
     </Box>
   )
