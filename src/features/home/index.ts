b75d55cd74f812aefb3b72d80ff9f9609fb3e107
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-17 21:16:24 +0200

    Exprt home api components from index #7

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,6 +1,11 @@
 export * from "./footer/Footer"
-export * from "./sections/FirstSection"
-export * from "./sections/SecondSection"
+export * from "./sections/LandingSection"
+export * from "./sections/LatestShows"
 export * from "./sections/ThirdSection"
+
+export * from "./api/LatestShowsApiConnector"
+export * from "./api/MoviesApiConnector"
+export * from "./api/SeriesApiConnector"
+export * from "./api/TrendingShowsApiConnector"
 
 export * from "./media/icons"
