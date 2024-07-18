09521619f2034e4a1c0e36c2c54e392ed2645ebf
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-18 22:13:18 +0200

    Create TrendingShows #7

--- src/features/home/sections/TrendingShows.tsx
+++ src/features/home/sections/TrendingShows.tsx
@@ -1,6 +1,19 @@
-
-export const TrendingShows = () => {
+import { Slider } from "@/components/ui/multi-slider/Slider"
+import { useTranslation } from "react-i18next"
+interface TrendingShowsProps {
+  isLoading:boolean
+}
+export const TrendingShows = ({isLoading}:TrendingShowsProps) => {
+  const {t} = useTranslation(["common"])
   return (
-    <div>TrendingShows</div>
+    <Slider
+    title={t("Trending Shows")}
+    needsTitle={true}
+    deskTopDisplayItems={9}
+    isLoading={isLoading}
+    cardSkeletonHeight="9rem"
+    marginLeft={1}
+    marginRight={1}
+  />
   )
 }
