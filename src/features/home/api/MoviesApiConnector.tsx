483e73ad9c8d9c925e61b0fbccbb75cf85744029
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-19 20:49:27 +0200

    Create MoviesApiConnector #7

--- src/features/home/api/MoviesApiConnector.tsx
+++ src/features/home/api/MoviesApiConnector.tsx
@@ -1,6 +1,11 @@
-
+import { useState } from "react"
+import { Movies } from "../sections/Movies"
 export const MoviesApiConnector = () => {
-  return (
-    <div>MoviesApiConnector</div>
-  )
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 1300)
+
+  return <Movies isLoading={isLoading} />
 }
