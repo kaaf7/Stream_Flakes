a37095f5dfe862e3f7c2ef4e7c80526d49f2caee
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-19 20:49:43 +0200

    Update AppBarContainer #7

--- src/components/ui/app-bar-container/AppBarContrainer.tsx
+++ src/components/ui/app-bar-container/AppBarContrainer.tsx
@@ -1,48 +1,58 @@
 import { AppBar, AppBarProps, Toolbar, useTheme } from "@mui/material"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
+
+import { debounce } from "lodash"
 
 export const AppBarContainer = ({ children }: AppBarProps) => {
   const [scrolled, setScrolled] = useState(false)
   const theme = useTheme()
 
+  const handleScroll = useCallback(
+    debounce(() => {
+      setScrolled(window.scrollY > 10)
+    }, 100),
+    []
+  )
+
   useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 20)
-    }
-
     window.addEventListener("scroll", handleScroll)
-
     return () => {
       window.removeEventListener("scroll", handleScroll)
     }
-  }, [])
+  }, [handleScroll])
+
+  const appBarStyles = useMemo(
+    () => ({
+      display: "flex",
+      position: "fixed",
+      width: "100%",
+      background: "yellow",
+      alignItems: "center",
+      backgroundColor: scrolled ? theme.palette.background.default : "transparent",
+      transition: "background-color 0.3s ease-in-out",
+      boxShadow: "none",
+      zIndex: 1000,
+      height: "4rem",
+    }),
+    [scrolled, theme.palette.background.default]
+  )
+
+  const toolbarStyles = useMemo(
+    () => ({
+      height: "100%",
+      width: "85%",
+      padding:"0 0 0 0",
+      display: "flex",
+      gap: 10,
+      alignItems: "center",
+      justifyContent: "space-between",
+    }),
+    []
+  )
 
   return (
-    <AppBar
-      sx={{
-        display: "flex",
-        direction: "column",
-        justifyContent: "space-between",
-        alignContent: "center",
-        justifyItems: "center",
-        alignItems: "center",
-        position: "fixed",
-        boxShadow: 0,
-        height: "4rem",
-        backgroundColor: scrolled ? theme.palette.background.default : "transparent",
-        transition: "background-color 0.3s ease-in-out"
-      }}>
-      <Toolbar
-        sx={{
-          display: "flex",
-          direction: "column",
-          position: "fixed",
-          alignContent: "center",
-          alignItems: "center",
-          gap: 3,
-          margin: 0,
-          width: "85vw"
-        }}>
+    <AppBar sx={appBarStyles}>
+      <Toolbar style={toolbarStyles}>
         {children}
       </Toolbar>
     </AppBar>
