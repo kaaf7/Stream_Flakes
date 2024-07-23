abcd01a41ca89c9347813601d665876e57790c0a
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-23 20:45:56 +0200

    Create FavoriteShowsApiConnector #18

---
+++ src/features/favorite-shows-grid/api/FavoriteShowsApiConnector.tsx
@@ -0,0 +1,10 @@
+import { ShowsFilterInterface } from "@/features/shows-filter"
+
+interface FavoriteShowsApiConnectorProps {
+    filter:ShowsFilterInterface
+}
+
+export const FavoriteShowsApiConnector = ({filter}:FavoriteShowsApiConnectorProps) => {
+
+  return <></>
+}
