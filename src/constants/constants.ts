453221fb81b53c46884968f394d7fcf2187ecb8f
Karim <108280260+kaaf7@users.noreply.github.com>
2024-07-28 18:50:00 +0200

    Add createUserAccountPath and new constants #18

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,7 +4,7 @@ export const USER_IDENTIFIER = ":userId"
 //** PATHS **/
 export const MAIN_PATH = "/"
 
-export const USER_PATH = "/user"
+export const USER_PATH = `/users/${USER_IDENTIFIER}`
 
 export const LOGIN_PATH = "/login"
 
@@ -25,7 +25,48 @@ export const createFavoriteShowsPath = (userId: string) => {
   return FAVORITES_PATH.replace(USER_IDENTIFIER, userId)
 }
 
+export const createUserAccountPath = (userId: string) => {
+  return USER_PATH.replace(USER_IDENTIFIER, userId)
+}
+
 /** SHOWS FILTERS **/
 export const SHOWS_FILTER_BY_NAME = "name"
 export const SHOWS_FILTER_BY_YEAR = "year"
 export const SHOWS_FILTER_BY_GENRE = "genre"
+
+//** TOOL TIP PLACEMENT */
+
+export enum ToolTipPlacement {
+  TOP = "top",
+  BOTTOM = "bottom",
+
+  LEFT = "left",
+  RIGHT = "right"
+}
+
+export enum MainColor {
+  PRIMARY = "primary",
+  SECONDRY = "secondry",
+
+  WARNING = "warning"
+}
+export enum ButtonSize {
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large"
+}
+export enum ButtonVariant {
+  OUTLINED = "outlined",
+  CONTAINED = "contained",
+  TEXT = "text"
+}
+export enum ChipSize {
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large"
+}
+export enum ChipVariant {
+  OUTLINED = "outlined",
+  CONTAINED = "contained",
+  TEXT = "text"
+}
