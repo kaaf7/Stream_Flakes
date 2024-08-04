9f34a33c8b26e781563d0e96272b520e467305a4
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-04 17:20:41 +0200

    Create new GridSkeleton and export it #18

---
+++ src/components/ui/grid-skeleton/GridSkeleton.tsx
@@ -0,0 +1,39 @@
+import { Box, Skeleton } from "@mui/material"
+
+interface GidSkeletonProps {
+  gridLength: number
+}
+export const GidSkeleton = ({ gridLength }: GidSkeletonProps) => {
+  return Array.from({ length: gridLength }, (_, index) => (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        overflow: "hidden",
+        borderRadius: "1rem"
+      }}>
+      <Skeleton
+        key={index}
+        variant="rectangular"
+        sx={{
+          width: "100%",
+          height: "100%"
+        }}>
+        <img
+          alt="shows-skeletons"
+          aria-label="shows-skeletons"
+          loading="lazy"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover"
+          }}
+          src={
+            "https://posters.movieposterdb.com/24_05/2024/21454134/l_the-bikeriders-movie-poster_95cd633e.jpg"
+          }
+        />
+      </Skeleton>
+    </Box>
+  ))
+}
