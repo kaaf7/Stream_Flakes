5d4fa0e8ebdf0639ee4d6603a3adaac6dec235dc
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-04 15:01:10 +0200

    Update ShowsGrid #18

--- src/features/shows-grid/components/ShowsGrid.tsx
+++ src/features/shows-grid/components/ShowsGrid.tsx
@@ -1,27 +1,58 @@
-import { MediaCard } from "@/components/ui/movie-card"
-import { SkeletonGrid } from "@/components/ui/skeleton-grid"
+import { ImageList, Skeleton } from "@mui/material"
+
+import { MediaCardUpdated } from "@/components/ui/media-card-updated"
+import { useScrollPagination } from "@/hooks/ifninite-scroll/useScrollPagination"
+import { useResponsive } from "@/hooks/responsive/useResponsive"
 import { Media } from "@/interfaces/Media"
-import { ImageList } from "@mui/material"
+import { useMemo } from "react"
 
 interface MediaGridProps {
   medias: Media[]
   isLoading: boolean
 }
 
+
 export const ShowsGrid = ({ medias, isLoading }: MediaGridProps) => {
-  if (isLoading) {
-    return <SkeletonGrid gridLength={28} minmax={"12.5rem"}/>
-  }
+  const { mobile,tablet } = useResponsive();
+  const { currentPage } = useScrollPagination()
+
+  const gridStyles = {
+    gridTemplateColumns: mobile || tablet ? "repeat(2, 1fr) !important" : "repeat(6, 1fr) !important",
+    width: "100%",
+    height: "100%",
+  };
+
+
+  const skeletons = useMemo(() => {
+    return Array.from({ length: 12 }, (_, index) => (
+      <Skeleton
+        key={index}
+        variant="rectangular"
+        sx={{
+          width: "100%",
+          //TODO change later
+          height: mobile || tablet ? "15rem": "25rem",
+          borderRadius: "1rem",
+        }}
+      />
+    ));
+  }, [mobile]);
+
   return (
-    <ImageList
-      gap={20}
-      sx={{
-        gridTemplateColumns: "repeat(auto-fill, minmax(12.5rem, 1fr)) !important",
-        width: "100%"
-      }}>
-      {medias.map((media: Media) => (
-        <MediaCard key={media.id} id={media.id} imageUrl={media.imageUrl} title={media.title} />
-      ))}
+    <ImageList gap={15} sx={gridStyles}>
+      {isLoading && currentPage === 0
+        ? skeletons
+        : medias.map((media: Media,index:number) => (
+            <MediaCardUpdated
+              borderRadius={"1rem"}
+              key={index}
+              id={media.id}
+              needsMediaCardBar={true}
+              isFavorite={true}
+              imageUrl={media.imageUrl}
+            />
+          ))}
+      {isLoading && currentPage > 0 && skeletons}
     </ImageList>
-  )
-}
+  );
+};
