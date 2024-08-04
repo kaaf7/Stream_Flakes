92d3b3b7c4475fb49fee403440bd780b101b59ee
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-04 18:11:12 +0200

    Add isFavorite to Media

--- src/interfaces/Media.ts
+++ src/interfaces/Media.ts
@@ -3,5 +3,6 @@ import { ImageListItemProps } from "@mui/material"
 export interface Media extends ImageListItemProps {
   id: string
   imageUrl: string
+  isFavorite?:boolean
   needsMediaCardBar?: boolean
 }
