4a92a5317334196dff34ad0f310475e20d493703
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-08 23:15:41 +0200

    Add Beadcrumbs to ShowsOverview #21

--- src/pages/shows/ShowsOverview.tsx
+++ src/pages/shows/ShowsOverview.tsx
@@ -1,20 +1,20 @@
 import { MAIN_PATH, MainColor, SHOWS_PATH } from "@/constants/constants"
+import { HomeMaxOutlined, KeyboardArrowRight, LocalMoviesOutlined } from "@mui/icons-material"
 import { Box, Breadcrumbs, Grid, Link } from "@mui/material"
 
 import { MediaCard } from "@/components/ui/movie-card"
 import { ShowsGridApiConnector } from "@/features/all-shows"
 import { BRAND_ICONS } from "@/features/home"
 import { useResponsive } from "@/hooks/responsive/useResponsive"
-import { KeyboardArrowRight } from "@mui/icons-material"
 import { useTranslation } from "react-i18next"
 
 export default function ShowsOverview() {
   const { t } = useTranslation(["common"])
   const { laptop, desktop } = useResponsive()
 
-  const BREAD_CRUMBS_ITEMS: { title: string; link: string }[] = [
-    { title: t("home"), link: MAIN_PATH },
-    { title: t("shows"), link: SHOWS_PATH }
+  const BREAD_CRUMBS_ITEMS: { icon: React.ReactNode; title: string; link: string }[] = [
+    { icon: <HomeMaxOutlined />, title: t("home"), link: MAIN_PATH },
+    { icon: <LocalMoviesOutlined />, title: t("shows"), link: SHOWS_PATH }
   ]
   return (
     <Box
@@ -29,41 +29,47 @@ export default function ShowsOverview() {
         gap: 2
       }}>
       <Box sx={{ width: "85%" }}>
-        <Breadcrumbs separator={<KeyboardArrowRight />} aria-label="breadcrumbs">
-          {BREAD_CRUMBS_ITEMS.map((item: { title: string; link: string }) => (
-            <Link key={item.title} color={MainColor.PRIMARY} href={item.link}>
-              {item.title}
-            </Link>
-          ))}
+        <Breadcrumbs
+          sx={{ fontSize: ".75rem" }}
+          separator={<KeyboardArrowRight />}
+          aria-label="breadcrumbs">
+          {BREAD_CRUMBS_ITEMS.map(
+            (item: { icon?: React.ReactNode; title: string; link: string }) => (
+              <Link key={item.title} color={MainColor.PRIMARY} href={item.link}>
+                {item.icon} {item.title}
+              </Link>
+            )
+          )}
         </Breadcrumbs>
       </Box>
       {laptop ||
         (desktop && (
-      <Box
-        sx={{
-          width: "85%",
-          height: "4rem",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center"
-        }}>
-        <Grid
-          container
-          sx={{
-            flex: 1,
-            gap: 1
-          }}>
-          {BRAND_ICONS.map((brandIcon) => (
-            <MediaCard
-              sx={{ width: "50px", height: "50px" }}
-              key={brandIcon}
-              imageUrl={brandIcon as string}
-              title={brandIcon as string}
-              id={brandIcon as string}
-            />
-          ))}
-        </Grid>
-      </Box>  ))}
+          <Box
+            sx={{
+              width: "85%",
+              height: "4rem",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center"
+            }}>
+            <Grid
+              container
+              sx={{
+                flex: 1,
+                gap: 1
+              }}>
+              {BRAND_ICONS.map((brandIcon) => (
+                <MediaCard
+                  sx={{ width: "50px", height: "50px" }}
+                  key={brandIcon}
+                  imageUrl={brandIcon as string}
+                  title={brandIcon as string}
+                  id={brandIcon as string}
+                />
+              ))}
+            </Grid>
+          </Box>
+        ))}
       <ShowsGridApiConnector />
     </Box>
   )
