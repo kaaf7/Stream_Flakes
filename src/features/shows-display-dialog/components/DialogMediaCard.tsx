5d078ec5a3f1390d37451ebaba7899d86d2f4cc0
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-10 17:05:51 +0200

    Format DialogMediaCard #21

--- src/features/shows-display-dialog/components/DialogMediaCard.tsx
+++ src/features/shows-display-dialog/components/DialogMediaCard.tsx
@@ -1,32 +1,32 @@
 import { Box } from "@mui/material"
 import { useState } from "react"
 
-export const  DialogMediaCard = ({ img }: { img: string })=> {
-    const [hover, setHover] = useState<boolean>(false)
-    return (
-      <Box
-        sx={{
+export const DialogMediaCard = ({ img }: { img: string }) => {
+  const [hover, setHover] = useState<boolean>(false)
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        overflow: "hidden",
+        borderRadius: "1rem"
+      }}
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}>
+      <img
+        src={img}
+        alt={img}
+        style={{
           width: "100%",
           height: "100%",
-          position: "relative",
-          overflow: "hidden",
-          borderRadius: "1rem"
+          objectFit: "cover",
+          opacity: hover ? 0.6 : 1,
+          cursor: hover ? "pointer" : "default",
+          transition: "opacity 0.5s ease-in-out"
         }}
-        onMouseOver={() => setHover(true)}
-        onMouseOut={() => setHover(false)}>
-        <img
-          src={img}
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            opacity: hover ? 0.6 : 1,
-            cursor: hover ? "pointer" : "default",
-            transition: "opacity 0.5s ease-in-out"
-          }}
-          alt={img}
-          loading="lazy"
-        />
-      </Box>
-    )
-  }
+        loading="lazy"
+      />
+    </Box>
+  )
+}
