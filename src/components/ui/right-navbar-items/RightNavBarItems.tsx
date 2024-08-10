e7037540ed436eb1ca218d6cba2ac35037a25034
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-10 17:01:57 +0200

    Add ShowsButtonWithDialogContainer  #21

--- src/components/ui/right-navbar-items/RightNavBarItems.tsx
+++ src/components/ui/right-navbar-items/RightNavBarItems.tsx
@@ -14,7 +14,7 @@ import { Box, ContainerProps } from "@mui/material"
 
 import { CustomButton } from "@/components/buttons/custom-button"
 import { CustomIconButton } from "@/components/buttons/icon-buttons/custom-icon-button"
-import { ShowsButtonWithDialog } from "@/features/shows-display-dialog"
+import { ShowsButtonWithDialogContainer } from "@/features/shows-display-dialog"
 import AccountCircle from "@mui/icons-material/AccountCircle"
 import { ReactNode } from "react"
 import { useTranslation } from "react-i18next"
@@ -42,7 +42,7 @@ export const RightNavBarItems = ({ isLoggedIn }: RightNavBarItemsProps) => {
     {
       isProtected: false,
       component: (
-        <ShowsButtonWithDialog/>
+        <ShowsButtonWithDialogContainer/>
       )
     },
     
