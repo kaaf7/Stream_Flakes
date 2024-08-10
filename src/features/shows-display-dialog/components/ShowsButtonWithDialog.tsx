8f5c64bb121d65466c18fd617ad412ed06194f46
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-10 17:06:09 +0200

    Update ShowsButtonWithDialog #21

--- src/features/shows-display-dialog/components/ShowsButtonWithDialog.tsx
+++ src/features/shows-display-dialog/components/ShowsButtonWithDialog.tsx
@@ -1,84 +1,43 @@
 import {
-    ButtonVariant,
-    MAIN_PATH,
-    MainColor,
-    SHOWS_PATH,
-    ToolTipPlacement
+  ButtonVariant,
+  MAIN_PATH,
+  MainColor,
+  SHOWS_PATH,
+  ToolTipPlacement
 } from "@/constants/constants"
-import { Box, Dialog, DialogActions, DialogContent, ImageList, ImageListItem } from "@mui/material"
+import { Dialog, DialogActions, DialogContent, ImageList, ImageListItem } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 
 import { CustomButton } from "@/components/buttons/custom-button"
 import { CustomIconButton } from "@/components/buttons/icon-buttons/custom-icon-button"
+import { DialogMediaCard } from "@/features/shows-display-dialog"
 import { LocalMoviesOutlined } from "@mui/icons-material"
 import { useTranslation } from "react-i18next"
 
-const itemData = [
-  {
-    img: "https://posters.movieposterdb.com/24_03/2024/22022452/l_inside-out-2-movie-poster_4b749fa4.jpg",
-    title: "Breakfast",
-    cols: 4,
-    rows: 2
-  },
-  {
-    img: "https://posters.movieposterdb.com/24_05/2024/21454134/l_the-bikeriders-movie-poster_95cd633e.jpg",
-    title: "Breakfast",
-    cols: 4,
-    rows: 2
-  },
-  {
-    img: "https://posters.movieposterdb.com/24_05/2024/21454134/l_the-bikeriders-movie-poster_95cd633e.jpg",
-    title: "Breakfast",
-    cols: 2
-  },
-  {
-    img: "https://posters.movieposterdb.com/24_05/2024/21454134/l_the-bikeriders-movie-poster_95cd633e.jpg",
-    title: "Breakfast",
-    cols: 2
-  },
-  {
-    img: "https://posters.movieposterdb.com/24_05/2024/21454134/l_the-bikeriders-movie-poster_95cd633e.jpg",
-    title: "Breakfast",
-    cols: 2
-  },
-  {
-    img: "https://posters.movieposterdb.com/24_05/2024/21454134/l_the-bikeriders-movie-poster_95cd633e.jpg",
-    title: "Breakfast",
-    cols: 2
-  },
-  {
-    img: "https://posters.movieposterdb.com/24_05/2024/21454134/l_the-bikeriders-movie-poster_95cd633e.jpg",
-    title: "Breakfast",
-    cols: 2
-  },
-  {
-    img: "https://posters.movieposterdb.com/24_05/2024/21454134/l_the-bikeriders-movie-poster_95cd633e.jpg",
-    title: "Breakfast",
-    cols: 2
-  },
-  {
-    img: "https://posters.movieposterdb.com/24_05/2024/21454134/l_the-bikeriders-movie-poster_95cd633e.jpg",
-    title: "Breakfast",
-    cols: 2
-  }
-]
-export const ShowsButtonWithDialog = () => {
+type DialogMedias = {
+  id:string,
+  imageUrl: string
+  cols?: number
+  rows?: number
+}
+
+interface ShowsButtonWithDialogProps {
+  medias: DialogMedias[]
+}
+export const ShowsButtonWithDialog = ({ medias }: ShowsButtonWithDialogProps) => {
   const { t } = useTranslation(["common"])
   const navigate = useNavigate()
   const location = useLocation()
-  const {  pathname } = location
+  const { pathname } = location
   const [isDialogOpen, setDialogOpen] = useState<boolean>(false)
 
-  const handleNavigation = () => {
-    navigate(SHOWS_PATH)
-  }
-
   const handleDialogState = () => {
-    if (pathname.endsWith(MAIN_PATH) ) {
+    if (location.pathname.endsWith(MAIN_PATH)) {
+      setDialogOpen(true)
+    } else {
       navigate(SHOWS_PATH)
     }
-    setDialogOpen(true)
   }
 
   useEffect(() => {
@@ -98,29 +57,28 @@ export const ShowsButtonWithDialog = () => {
       <Dialog
         open={isDialogOpen}
         onClose={() => setDialogOpen(false)}
-        sx={{ height: "100%", overflowY: "hidden", padding: 0 }}
-        fullWidth
+        sx={{ overflowY: "hidden", padding: 0 }}
         maxWidth="tablet">
         <DialogContent>
           <ImageList
-            sx={{ width: "100%", height: "100%", overflowY: "hidden" }}
+            sx={{ width: "30rem", height: "30rem", overflowY: "hidden" }}
             variant="quilted"
             gap={5}
             cols={10}>
-            {itemData.map((item) => (
-              <ImageListItem key={item.img} rows={item.rows ?? 1} cols={item.cols || 1}>
-                <DialogMediaCard img={item.img} />
+            {medias.map((media) => (
+              <ImageListItem key={media.id} rows={media.rows ?? 1} cols={media.cols || 1}>
+                <DialogMediaCard img={media.imageUrl} />
               </ImageListItem>
             ))}
           </ImageList>
         </DialogContent>
         <DialogActions
           sx={{
-            padding: "20px ",
+            padding: "1rem",
             justifyContent: "center"
           }}>
           <CustomButton
-            onClick={handleNavigation}
+            onClick={() => navigate(SHOWS_PATH)}
             sx={{ width: "100%" }}
             variant={ButtonVariant.OUTLINED}>
             {t("explore")}
@@ -128,35 +86,5 @@ export const ShowsButtonWithDialog = () => {
         </DialogActions>
       </Dialog>
     </>
-  )
-}
-
-function DialogMediaCard({ img }: { img: string }) {
-  const [hover, setHover] = useState<boolean>(false)
-  return (
-    <Box
-      sx={{
-        width: "100%",
-        height: "100%",
-        position: "relative",
-        overflow: "hidden",
-        borderRadius: "1rem"
-      }}
-      onMouseOver={() => setHover(true)}
-      onMouseOut={() => setHover(false)}>
-      <img
-        src={img}
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          opacity: hover ? 0.6 : 1,
-          cursor: hover ? "pointer" : "default",
-          transition: "opacity 0.5s ease-in-out"
-        }}
-        alt={img}
-        loading="lazy"
-      />
-    </Box>
   )
 }
