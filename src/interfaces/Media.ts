50dac419a8705058edd224311c4cba8efa32f77d
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-16 23:40:27 +0200

    Rename Media to MediaCardProps #16

--- src/interfaces/Media.ts
+++
@@ -1,8 +0,0 @@
-import { ImageListItemProps } from "@mui/material"
-
-export interface Media extends ImageListItemProps {
-  id: string
-  imageUrl: string
-  isFavorite?:boolean
-  needsMediaCardBar?: boolean
-}
