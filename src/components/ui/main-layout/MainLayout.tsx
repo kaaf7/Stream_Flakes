d69bb3319a9c41f8553b8333a1a6897c0285efaa
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-17 21:27:35 +0200

    Add BasdContainer and Footer to MainLayout #16

--- src/components/ui/main-layout/MainLayout.tsx
+++ src/components/ui/main-layout/MainLayout.tsx
@@ -1,7 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom"
 
+import { Footer } from "@/components/ui/main-layout-footer"
 import { NavBar } from "@/components/ui/nav-bar"
 import { LOGIN_PATH } from "@/constants/constants"
+import { BaseContainer } from "../base-container"
 
 interface MainLayoutProps {
   isLoggedIn: boolean
@@ -9,10 +11,11 @@ interface MainLayoutProps {
 
 export function MainLayout({ isLoggedIn }: MainLayoutProps) {
   return isLoggedIn ? (
-    <>
+    <BaseContainer>
       <NavBar isLoggedIn />
       <Outlet />
-    </>
+      <Footer />
+    </BaseContainer>
   ) : (
     <Navigate to={LOGIN_PATH} />
   )
