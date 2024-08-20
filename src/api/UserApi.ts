78b612f98fbb8c1c64a4b1dd5f3e848ea7abf1ad
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-20 21:46:40 +0200

    Create UserApi #2

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -1,3 +1,41 @@
-export class UserApir {
-    
+import { BaseApi } from "./BaseApi"
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+interface LoginInfo {
+  username: string
+  password: string
+}
+
+interface RegisterInfo {
+  username: string
+  password: string
+}
+
+interface FavoriteMediasBody {
+  favorites: string[]
+}
+export class UserApi extends BaseApi {
+  resgister = async (registerInfo: RegisterInfo) => {
+    return await fetch(`${BASE_URL}/auth/register`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(registerInfo)
+    })
+  }
+  login = async (loginInfo: LoginInfo) => {
+    return await fetch(`${BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(loginInfo)
+    })
+  }
+
+  upsertUserFavoriteMedias = async (userId: string, body: FavoriteMediasBody) => {
+    return await fetch(`${BASE_URL}/user/${userId}/favorites/update`, {
+      method: "PUT",
+      headers: this.headers,
+      body: JSON.stringify(body)
+    })
+  }
 }
