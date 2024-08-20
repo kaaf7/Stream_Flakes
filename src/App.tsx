1e911bf2b97176559edca2081ae3bc9299213de1
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-20 21:43:41 +0200

    Remove AuthProvider from App #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import "./App.css"
 
 import { CssBaseline, ThemeProvider } from "@mui/material"
-import { useState } from "react"
 
 import { Router } from "@/components/router/Router"
 import englishTranslation from "@/translations/en/english.json"
 import { defineTheme } from "@/utils/theme-creator/themeCreator"
 import i18next from "i18next"
+import { useState } from "react"
 import { I18nextProvider } from "react-i18next"
-import AuthProvider from "./components/Auth/AuthProvider"
 
 function App() {
   const [isDarkModOn, setDarkModeOn] = useState<boolean>(true)
-  const theme = defineTheme(isDarkModOn)
 
+  const theme = defineTheme(isDarkModOn)
   i18next.init({
     interpolation: { escapeValue: false },
     lng: "en",
@@ -24,14 +23,12 @@ function App() {
     }
   })
   return (
-    <AuthProvider>
-      <I18nextProvider i18n={i18next}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Router isLoggedIn />
-        </ThemeProvider>
-      </I18nextProvider>
-    </AuthProvider>
+    <I18nextProvider i18n={i18next}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router isLoggedIn />
+      </ThemeProvider>
+    </I18nextProvider>
   )
 }
 
