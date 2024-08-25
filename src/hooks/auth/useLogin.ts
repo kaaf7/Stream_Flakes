5d190d986e12538a54a9f3bceb7a43e5a6b4a1d4
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-25 18:53:48 +0200

    Add interafce LoginInfo to userLogin #2

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -3,7 +3,7 @@ import { UserApi } from "@/api/UserApi";
 export const useLogin = () => {
   const authApi = new UserApi(null)
   
-  const submitLogin =  (loginInfo: { username: string; password: string }) => {
+  const submitLogin =  (loginInfo: { username: string|null; password: string|null }) => {
     return authApi.login(loginInfo)
   }
 
