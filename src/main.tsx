2d0f219c46dadcdbaa9a5b57aa69c35b9ad12ab9
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-25 17:58:09 +0200

    Add AuthProvider #2

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import "./index.css"
 
-import App from "./App.tsx"
 import React from "react"
 import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import AuthProvider from "./components/Auth/AuthProvider.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <>
+    <AuthProvider>
       <App />
-    </>
+    </AuthProvider>
   </React.StrictMode>
 )
