e6206f6f5611a712a63bab6c46958928d87b9d52
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-25 21:23:30 +0200

    Update serializeParams filter #5

--- src/helpers/serializeParams.ts
+++ src/helpers/serializeParams.ts
@@ -1,11 +1,15 @@
 export const serializeParams = (
-    filterName: string,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    filter: any
-  ) => {
-    const validFilterEntries = Object.entries(filter)
-      .filter(([k, v]) => ["string", "number", "boolean"].includes(typeof v) && v !== "")
-      .map(([k, v]) => `${k}=${v}`).join("&")
-  
-    return validFilterEntries.length > 0 && `${filterName}?${validFilterEntries}`
-  }
+  filterName: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  filter: any
+) => {
+  const validFilterEntries = Object.entries(filter)
+    .filter(
+      ([k, v]) =>
+        ["string"].includes(typeof k) && ["string", "number", "boolean"].includes(typeof v) && v !== ""
+    )
+    .map(([k, v]) => `${k}=${v}`)
+    .join("&")
+
+  return validFilterEntries.length > 0 && `${filterName}?${validFilterEntries}`
+}
