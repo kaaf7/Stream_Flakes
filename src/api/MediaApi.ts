67a60adca83d81d3f18e50f4efd22cd4224d8328
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-25 21:12:57 +0200

    Create MediaApi #5

---
+++ src/api/MediaApi.ts
@@ -0,0 +1,14 @@
+import { MediasFilterInterface } from "@/features/shows-filter"
+import { serializeParams } from "@/helpers/serializeParams"
+import { BaseMediaApi } from "./BaseMediaApi"
+const MEDIAS_URL = import.meta.env.VITE_MEDIAS_URL
+
+export class MediaApi extends BaseMediaApi {
+  getMedias = async (filterParams: MediasFilterInterface) => {
+    const requestParams = serializeParams("Filter", filterParams)
+    return fetch(`${MEDIAS_URL}/${requestParams}`, {
+      method: "GET",
+      headers : this.headers
+    })
+  }
+}
