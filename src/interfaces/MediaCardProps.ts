af6874dade69de6aead5540b5b5c87cda84b4e7e
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-28 21:18:04 +0200

    Add all needed MediaCardProps  #5

--- src/interfaces/MediaCardProps.ts
+++ src/interfaces/MediaCardProps.ts
@@ -1,8 +1,31 @@
-import { ImageListItemProps } from "@mui/material"
+import { ImageListItemProps } from "@mui/material";
 
 export interface MediaCardProps  extends ImageListItemProps {
-  id: string
-  imageUrl: string
-  isFavorite?:boolean
-  needsMediaCardBar?: boolean
+  _id: string;
+  id: string;
+  imageUrl?: string;
+  isFavorite?: boolean;
+  needsMediaCardBar?: boolean;
+  adult?: boolean;
+  backdrop_path?: string;
+  budget?: number;
+  genres?: string;
+  homepage?: string;
+  imdb_id?: string;
+  original_language?: string;
+  original_title?: string;
+  overview?: string;
+  popularity?: number;
+  poster_path?: string;
+  production_companies?: string;
+  production_countries?: string;
+  release_date?: string; // Using string for ISO 8601 date format
+  revenue?: number;
+  runtime?: number;
+  spoken_languages?: string;
+  status?: string;
+  tagline?: string;
+  title?: string;
+  vote_average?: number;
+  vote_count?: number;
 }
