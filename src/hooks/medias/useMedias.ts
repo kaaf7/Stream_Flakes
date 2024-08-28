55be2a65b0cf321800e8df22e9aa65f4847f3d84
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-28 22:16:18 +0200

    Add limit to useMedias #5

--- src/hooks/medias/useMedias.ts
+++ src/hooks/medias/useMedias.ts
@@ -1,32 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react"
 
 import { MediaApi } from "@/api/MediaApi"
 import { MediasFilterInterface } from "@/features/shows-filter"
-import { formatResponse } from "@/helpers/formatResponse"
 
-export const useMedias = (mediaFilterParams: MediasFilterInterface) => {
+interface UseMediaProps {
+    mediaFilterParams: MediasFilterInterface
+    limit:number
+}
+
+export const useMedias = ({mediaFilterParams,limit}: UseMediaProps) => {
   const mediaApi = new MediaApi()
   const [isLoading, setLoading] = useState<boolean>(true)
-  const [response, setResponse] = useState<unknown>(null)
+  const [response, setResponse] = useState<any>(null)
   const [refresh, setRefresh] = useState<boolean>(true)
   const [errors, setErrors] = useState<unknown>(null)
   const refetch = () => setRefresh(true)
-
-  useEffect(()=>{
+  useEffect(() => {
     refetch()
-  },[mediaFilterParams])
+  }, [mediaFilterParams,limit])
 
   useEffect(() => {
-    if(!refresh){
-        return
+    if (!refresh) {
+      return
     }
 
     const fetchData = async () => {
       setLoading(true)
       try {
-        const response = await mediaApi.getMedias(mediaFilterParams)
+        const response = await mediaApi.getMedias( mediaFilterParams,limit)
         if (response && response.ok) {
-          setResponse(formatResponse(response))
+          const data = await response.json()
+          setResponse(data)
         }
       } catch (error) {
         console.error(error)
