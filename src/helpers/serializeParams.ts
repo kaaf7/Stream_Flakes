4c8e400f61c1365a96da9f0a4e03eaebe0b59075
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-28 22:13:59 +0200

    Add limit to serializeParams #5

--- src/helpers/serializeParams.ts
+++ src/helpers/serializeParams.ts
@@ -1,15 +1,25 @@
 export const serializeParams = (
+  limit:number,
   filterName: string,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  filter: any
-) => {
+  filter:any
+): string => {
   const validFilterEntries = Object.entries(filter)
-    .filter(
-      ([k, v]) =>
-        ["string"].includes(typeof k) && ["string", "number", "boolean"].includes(typeof v) && v !== ""
-    )
-    .map(([k, v]) => `${k}=${v}`)
-    .join("&")
+  .filter(
+    ([k, v]) =>
+      typeof k === "string" &&
+      ["string", "number", "boolean"].includes(typeof v) &&
+      v !== ""
+  )
+  .map(([k, v]) => {
+    const capitalizedKey = k.charAt(0).toUpperCase() + k.slice(1);
+    return `${capitalizedKey}=${v}`;
+  })
+  .join("&");
 
-  return validFilterEntries.length > 0 && `${filterName}?${validFilterEntries}`
-}
+  if (validFilterEntries.length === 0) {
+    return `${filterName}?Limit=${limit}`
+  }
+
+  return `${filterName}?Limit=${limit}&${validFilterEntries}`;
+};
