a6e366e8d0be347a4b0708cbdc41091908d23818
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-28 22:16:31 +0200

    Add limit to MediaApi #5

--- src/api/MediaApi.ts
+++ src/api/MediaApi.ts
@@ -4,8 +4,8 @@ import { BaseMediaApi } from "./BaseMediaApi"
 const MEDIAS_URL = import.meta.env.VITE_MEDIAS_URL
 
 export class MediaApi extends BaseMediaApi {
-  getMedias = async (filterParams: MediasFilterInterface) => {
-    const requestParams = serializeParams("Filter", filterParams)
+  getMedias = async (filterParams: MediasFilterInterface,limit:number) => {
+    const requestParams = serializeParams(limit,"Filter", filterParams)
     return fetch(`${MEDIAS_URL}/${requestParams}`, {
       method: "GET",
       headers : this.headers
