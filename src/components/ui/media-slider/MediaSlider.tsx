5bfa0c9db52853a7059754cd98f5a112132c2c79
Karim <108280260+kaaf7@users.noreply.github.com>
2024-08-31 19:47:59 +0200

    Move and Update MediaSlider #5

--- src/components/ui/media-slider/MediaSlider.tsx
+++ src/components/ui/media-slider/MediaSlider.tsx
@@ -1,10 +1,11 @@
 import { Box, Button } from "@mui/material"
 import { useEffect, useRef } from "react"
 
-import { TmdbImageSizes } from "@/constants/constants"
-import { MediaCardProps } from "@/interfaces/MediaCardProps"
+import { TmdbImageSizes } from "@/constants/constants.ts"
+import { MediaCardProps } from "@/interfaces/MediaCardProps.ts"
 import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined"
 import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined"
+import { GidSkeleton } from "@/components/ui/grid-skeleton"
 
 interface MoviesProps {
   isLoading: boolean
@@ -13,25 +14,26 @@ interface MoviesProps {
   cardWidth?: number
   cardHeight?: string
   cardCount?: number
-  slideTimer?:number
+  slideTimer?: number
 }
 
 export const MediaSlider = ({
   isLoading,
   medias,
-  containerHeight = "55vh",
+  containerHeight = "52vh",
   cardWidth = 14.2,
-  cardHeight="50vh",
+  cardHeight = "50vh",
   cardCount = 5,
-  slideTimer=5000
+  slideTimer = 10000
 }: MoviesProps) => {
   const scrollRef = useRef(null)
 
   const gapWidth = 1
-  const scrollOffset = cardWidth * cardCount + gapWidth * cardCount
+  const scrollOffset = cardWidth * (cardCount - 2) + gapWidth * (cardCount - 2)
   const scrollOffsetPx = scrollOffset * 16
 
   const handleSlide = (direction: number) => {
+    // @ts-ignore
     scrollRef?.current?.scrollBy({ left: direction * scrollOffsetPx, behavior: "smooth" })
   }
   useEffect(() => {
@@ -46,7 +48,6 @@ export const MediaSlider = ({
       sx={{
         position: "relative",
         width: "100%",
-        maxWidth: "75rem",
         height: containerHeight,
         display: "flex",
         alignItems: "center",
@@ -61,7 +62,9 @@ export const MediaSlider = ({
           zIndex: 100,
           left: "-5rem",
           top: "50%",
-          transform: "translateY(-50%)"
+          transform: "translateY(-50%)",
+          opacity: 0.5
+
         }}
         onClick={() => handleSlide(-1)}>
         <ArrowLeftOutlinedIcon />
@@ -81,37 +84,46 @@ export const MediaSlider = ({
           borderRadius: "1rem",
           position: "relative"
         }}>
-        {medias?.map((media: MediaCardProps) => (
-          <img
-            id={media.id}
-            alt={media.title}
-            src={media?.poster_path?.replace("original", TmdbImageSizes.LOGO_W500)}
-            key={media.id}
-            style={{
-              width: `${cardWidth}rem`,
-              height: cardHeight,
-              borderRadius: "1rem",
-              flexShrink: 0,
-              objectFit: "cover"
-            }}
+        {isLoading ? (
+          <GidSkeleton
+            gridLength={cardCount}
+            skeletonWidth={`${cardWidth}rem`}
+            skeletonHeight={cardHeight}
           />
-        ))}
+        ) : (
+          medias?.map((media: MediaCardProps) => (
+            <img
+              id={media.id}
+              alt={media.title}
+              src={media?.poster_path?.replace(TmdbImageSizes.POSTER_ORIGINAL, TmdbImageSizes.POSTER_W780)}
+              key={media.id}
+              style={{
+                width: `${cardWidth}rem`,
+                height: cardHeight,
+                borderRadius: "1rem",
+                flexShrink: 0,
+                objectFit: "cover"
+              }}
+            />
+          ))
+        )}
       </Box>
       <Button
         sx={{
-          width: "1rem", // Set the same width as height to make it a circle
-          height: cardHeight, // Set a fixed height
+          width: "1rem",
+          height: cardHeight,
           position: "absolute",
           zIndex: 100,
           right: "-5rem",
           top: "50%",
-          transform: "translateY(-50%)", // Adjust for centering vertically
-          display: "flex", // Center the content inside the button
+          transform: "translateY(-50%)",
+          display: "flex",
           alignItems: "center",
-          justifyContent: "center"
+          justifyContent: "center",
+          opacity: 0.5
         }}
         onClick={() => handleSlide(1)}>
-        <ArrowRightOutlinedIcon fontSize="large" />
+        <ArrowRightOutlinedIcon />
       </Button>
     </Box>
   )
