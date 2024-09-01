a0f7947eb38d41bd1b30bb4255a673c289f74437
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-01 13:30:36 +0200

    Cleanup of formatResponse #5

--- src/helpers/formatResponse.ts
+++ src/helpers/formatResponse.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const formatResponse = async (res: any) => {
-    if (res.ok) {
-      return await res.json()
-    }
-    return null
+  if (res.ok) {
+    return await res.json()
   }
+  return null
+}
