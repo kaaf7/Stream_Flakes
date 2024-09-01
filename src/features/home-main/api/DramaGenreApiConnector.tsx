f53f349032b4150e6c0751ece93c4600107ab624
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-01 13:25:36 +0200

    Cleanup of  DramaGenreApiConnector #5

--- src/features/home-main/api/DramaGenreApiConnector.tsx
+++ src/features/home-main/api/DramaGenreApiConnector.tsx
@@ -1,19 +1,28 @@
 import { MediaSlider } from "@/features/home-main"
 import { MediasFilterInterface } from "@/features/shows-filter"
 import { useMedias } from "@/hooks/medias/useMedias"
-import { Genre } from "@/interfaces/Genre"
+import { Genre, MediaResultLimit } from "@/interfaces/Genre"
 import { useState } from "react"
+import { Languages } from "@/constants/constants.ts"
+import { useResponsive } from "@/hooks/responsive/useResponsive.ts"
 
-const GENRE_LIST = [Genre.COMEDY, Genre.ACTION, Genre.DRAMA]
-
-const trendingFilterStat = {
-  originalLnguage:"en",
-  spokenLanguage:"en",
+const trendingFilterState = {
+  originalLanguage: Languages.ENGLISH,
+  spokenLanguage: Languages.ENGLISH,
   genre: Genre.DRAMA
 }
 export const DramaGenreApiConnector = () => {
-  const [filter, setFilter] = useState<MediasFilterInterface>(trendingFilterStat)
+  const [filter] = useState<MediasFilterInterface>(trendingFilterState)
+  const { mobile } = useResponsive()
 
-  const { isLoading, response: medias, errors } = useMedias({ mediaFilterParams: filter, limit: 50 })
-  return <MediaSlider cardCount={5} cardWidth={14.2} isLoading={isLoading} medias={medias} slideTimer={12000} />
+  const { isLoading, response: medias } = useMedias({ mediaFilterParams: filter,     limit: MediaResultLimit.LIMIT_50})
+  return (
+    <MediaSlider
+      cardCount={mobile?1:5}
+      cardWidth={14.2}
+      isLoading={isLoading}
+      medias={medias}
+      slideTimer={12000}
+    />
+  )
 }
