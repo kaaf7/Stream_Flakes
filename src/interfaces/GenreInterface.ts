b3be8e5464a4b2d34d006c4a3e334ddd1f4112c7
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-04 20:59:12 +0200

    Update and Rename GenreInterface #5

---
+++ src/interfaces/GenreInterface.ts
@@ -0,0 +1,30 @@
+export enum GenreInterface {
+  ACTION = "action",
+  ADVENTURE = "adventure",
+  ANIMATION = "animation",
+  COMEDY = "comedy",
+  CRIME = "crime",
+  DOCUMENTARY = "documentary",
+  DRAMA = "drama",
+  FAMILY = "family",
+  FANTASY = "fantasy",
+  HISTORICAL = "historical",
+  HORROR = "horror",
+  MUSIC = "music",
+  MYSTERY = "mystery",
+  ROMANCE = "romance",
+  SCIENCE_FICTION = "science fiction",
+  TV_MOVIE = "tv movie",
+  THRILLER = "thriller",
+  WAR = "war",
+  WESTERN = "western"
+}
+
+export enum MediaResultLimit {
+  LIMIT_6 = 6,
+
+  LIMIT_25 = 25,
+  LIMIT_50 = 50,
+  LIMIT_75 = 75,
+  LIMIT_100 = 100
+}
