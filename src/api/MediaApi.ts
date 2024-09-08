6a0395ec2736d11e354a21469fc0ff141d934bb1
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-08 17:34:35 +0200

    Add searchMediaByTitle & findMediaByImdbId  #5

--- src/api/MediaApi.ts
+++ src/api/MediaApi.ts
@@ -1,15 +1,28 @@
 import { MediasFilterInterface } from "@/features/shows-filter"
 import { serializeParams } from "@/helpers/serializeParams"
 import { BaseMediaApi } from "./BaseMediaApi"
+
 const MEDIAS_URL = import.meta.env.VITE_MEDIAS_URL
 
 export class MediaApi extends BaseMediaApi {
-  getMedias = async (filterParams: MediasFilterInterface,limit:number) => {
-    const requestParams = serializeParams(limit,"Filter", filterParams)
+  getMedias = async (filterParams: MediasFilterInterface, limit: number) => {
+    const requestParams = serializeParams(limit, "Filter", filterParams)
     return fetch(`${MEDIAS_URL}/${requestParams}`, {
       method: "GET",
-      headers : this.headers
+      headers: this.headers
     })
   }
 
+  searchMediaByTitle = async (title: string) => {
+    return fetch(`${MEDIAS_URL}/Search/${title}`, {
+      method: "GET",
+      headers: this.headers
+    })
+  }
+  findMediaByImdbId = async (imdbId: string) => {
+    return fetch(`${MEDIAS_URL}/FindByImbdId/${imdbId}`, {
+      method: "GET",
+      headers: this.headers
+    })
+  }
 }
