1693b802451a6f05cf18c7c8d813b5db142c8b41
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-08 18:46:53 +0200

    Add responsiveness to ActionGenreApiConnector #5

--- src/features/home-main/api/ActionGenreApiConnector.tsx
+++ src/features/home-main/api/ActionGenreApiConnector.tsx
@@ -1,27 +1,28 @@
+import { Languages } from "@/constants/constants.ts"
 import { MediaSlider } from "@/features/home-main"
 import { MediasFilterInterface } from "@/features/shows-filter"
 import { useMedias } from "@/hooks/medias/useMedias"
-import { Genre, MediaResultLimit } from "@/interfaces/Genre"
-import { useState } from "react"
-import { Languages } from "@/constants/constants.ts"
 import { useResponsive } from "@/hooks/responsive/useResponsive.ts"
+import { GenreInterface, MediaResultLimit } from "@/interfaces/GenreInterface.ts"
+import { useState } from "react"
 
 const trendingFilterState = {
   originalLanguage: Languages.ENGLISH,
   spokenLanguage: Languages.ENGLISH,
-  genre: Genre.ACTION
+  genre: GenreInterface.ACTION
 }
 export const ActionGenreApiConnector = () => {
   const [filter] = useState<MediasFilterInterface>(trendingFilterState)
-  const { mobile } = useResponsive()
+  const { mobile, tablet, laptop, desktop } = useResponsive()
   const { isLoading, response: medias } = useMedias({
     mediaFilterParams: filter,
-    limit: MediaResultLimit.LIMIT_50
+    limit: MediaResultLimit.LIMIT_10
   })
   return (
     <MediaSlider
-      cardCount={mobile?1:5}
-      cardWidth={14.2}
+      cardCount={mobile ? 2 : tablet ? 3 : laptop ? 3 : 6}
+      cardWidth={desktop ? 25 : 14.2}
+      containerHeight={"40vh"}
       isLoading={isLoading}
       medias={medias}
       slideTimer={15000}
