b2b43b26605b9e7b97dcdca4c529296cedcb0a4a
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-14 10:22:43 +0200

    Add responsive to TrendingShowsApiConnector #5

--- src/features/home-main/api/TrendingShowsApiConnector.tsx
+++ src/features/home-main/api/TrendingShowsApiConnector.tsx
@@ -1,24 +1,35 @@
+import { Languages } from "@/constants/constants.ts"
 import { MediaSlider } from "@/features/home-main"
 import { MediasFilterInterface } from "@/features/shows-filter"
+import { getRandomGenre } from "@/helpers/getRandomGenre.ts"
 import { useMedias } from "@/hooks/medias/useMedias"
-import { Genre } from "@/interfaces/Genre"
+import { useResponsive } from "@/hooks/responsive/useResponsive.ts"
+import { GenreInterface, MediaResultLimit } from "@/interfaces/GenreInterface.ts"
 import { useState } from "react"
 
-const GENRE_LIST = [Genre.COMEDY, Genre.ACTION, Genre.DRAMA]
-
-function getRandomGenre() {
-  const randomIndex = Math.floor(Math.random() * GENRE_LIST.length)
-  return GENRE_LIST[randomIndex]
-}
-const trendingFilterStat = {
-  originalLnguage:"en",
-  spokenLanguage:"en",
-  genre: getRandomGenre()
+const GENRE_LIST = [GenreInterface.TV_MOVIE, GenreInterface.DOCUMENTARY]
+const filterState = {
+  originalLanguage: Languages.ENGLISH,
+  spokenLanguage: Languages.ENGLISH,
+  genre: getRandomGenre(GENRE_LIST)
 }
 export const TrendingShowsApiConnector = () => {
-  const [filter, setFilter] = useState<MediasFilterInterface>(trendingFilterStat)
+  const [filter] = useState<MediasFilterInterface>(filterState)
+  const { mobile, tablet, laptop } = useResponsive()
 
-  const { isLoading, response: medias, errors } = useMedias({ mediaFilterParams: filter, limit: 50 })
+  const { isLoading, response: medias } = useMedias({
+    mediaFilterParams: filter,
+    limit: MediaResultLimit.LIMIT_10
+  })
 
-  return <MediaSlider cardCount={10} cardWidth={7.1} cardHeight="25VH"  containerHeight="30vh" isLoading={isLoading} medias={medias}slideTimer={25000} />
+  return (
+    <MediaSlider
+      cardCount={mobile ? 2 : tablet ? 4 : laptop ? 5 : 10}
+      cardWidth={7.4}
+      containerHeight={mobile ? "40vh" : tablet ? "40vh" : "25vh"}
+      isLoading={isLoading}
+      medias={medias}
+      slideTimer={25000}
+    />
+  )
 }
