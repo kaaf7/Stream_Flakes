a97d1a95a83bbaa392b732b4ad6d8e16748a5abc
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-14 10:23:26 +0200

    Add respnsive to ShowsButtonWithDialog #5

--- src/features/medias-display-dialog/components/ShowsButtonWithDialog.tsx
+++ src/features/medias-display-dialog/components/ShowsButtonWithDialog.tsx
@@ -1,43 +1,43 @@
+import { CustomButton } from "@/components/buttons/custom-button"
+import { CustomIconButton } from "@/components/buttons/icon-buttons/custom-icon-button"
+import { GidSkeleton } from "@/components/ui/grid-skeleton"
+import { MediaCard } from "@/components/ui/media-card"
 import {
   ButtonVariant,
+  createMediaPath,
   MAIN_PATH,
   MainColor,
   SHOWS_PATH,
+  TmdbImageSizes,
   ToolTipPlacement
 } from "@/constants/constants"
+import { useResponsive } from "@/hooks/responsive/useResponsive.ts"
+import { MediaCardProps } from "@/interfaces/MediaCardProps.ts"
+import { LocalMoviesOutlined } from "@mui/icons-material"
 import { Dialog, DialogActions, DialogContent, ImageList, ImageListItem } from "@mui/material"
-import { useEffect, useState } from "react"
+import { SyntheticEvent, useEffect, useState } from "react"
+import { useTranslation } from "react-i18next"
 import { useLocation, useNavigate } from "react-router-dom"
 
-import { CustomButton } from "@/components/buttons/custom-button"
-import { CustomIconButton } from "@/components/buttons/icon-buttons/custom-icon-button"
-import { DialogMediaCard } from "@/features/medias-display-dialog"
-import { LocalMoviesOutlined } from "@mui/icons-material"
-import { useTranslation } from "react-i18next"
-
-type DialogMedias = {
-  id:string,
-  imageUrl: string
-  cols?: number
-  rows?: number
-}
-
 interface ShowsButtonWithDialogProps {
-  medias: DialogMedias[]
+  isLoading: boolean
+  medias: MediaCardProps[]
 }
-export const ShowsButtonWithDialog = ({ medias }: ShowsButtonWithDialogProps) => {
+
+export const ShowsButtonWithDialog = ({ isLoading, medias }: ShowsButtonWithDialogProps) => {
   const { t } = useTranslation(["common"])
   const navigate = useNavigate()
   const location = useLocation()
+  const { mobile } = useResponsive()
   const { pathname } = location
   const [isDialogOpen, setDialogOpen] = useState<boolean>(false)
 
   const handleDialogState = () => {
-    if (location.pathname.endsWith(MAIN_PATH)) {
+    if (location.pathname.endsWith(MAIN_PATH) && !mobile) {
       setDialogOpen(true)
-    } else {
-      navigate(SHOWS_PATH)
+      return
     }
+    navigate(SHOWS_PATH)
   }
 
   useEffect(() => {
@@ -61,15 +61,31 @@ export const ShowsButtonWithDialog = ({ medias }: ShowsButtonWithDialogProps) =>
         maxWidth="tablet">
         <DialogContent>
           <ImageList
-            sx={{ width: "30rem", height: "30rem", overflowY: "hidden" }}
+            sx={{ width: "30rem", height: "25rem", overflowY: "hidden" }}
             variant="quilted"
             gap={5}
-            cols={10}>
-            {medias.map((media) => (
-              <ImageListItem key={media.id} rows={media.rows ?? 1} cols={media.cols || 1}>
-                <DialogMediaCard img={media.imageUrl} />
-              </ImageListItem>
-            ))}
+            cols={4}>
+            {isLoading ? (
+              <GidSkeleton gridLength={8} />
+            ) : (
+              medias?.map((media) => (
+                <ImageListItem key={media?.id}>
+                  <MediaCard
+                    onClick={(event: SyntheticEvent) => {
+                      event.preventDefault()
+                      navigate(createMediaPath(media.imdb_id as string))
+                    }}
+                    id={media?.id}
+                    imageUrl={
+                      media?.poster_path?.replace(
+                        TmdbImageSizes.POSTER_ORIGINAL,
+                        TmdbImageSizes.POSTER_W342
+                      ) as string as string
+                    }
+                  />
+                </ImageListItem>
+              ))
+            )}
           </ImageList>
         </DialogContent>
         <DialogActions
