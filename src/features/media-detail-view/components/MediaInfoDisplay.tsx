d0358d2f2f7714dde27436185322520754a23eb4
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-15 21:44:23 +0200

    Update MediaInfoDisplay and add onAddToFavorites #5

--- src/features/media-detail-view/components/MediaInfoDisplay.tsx
+++ src/features/media-detail-view/components/MediaInfoDisplay.tsx
@@ -1,46 +1,106 @@
-import { ButtonVariant, MainColor } from "@/constants/constants"
+import { CustomButton } from "@/components/buttons/custom-button"
+import { MediaCard } from "@/components/ui/media-card"
+
+import { createBackDropBack, MainColor, TmdbImageSizes } from "@/constants/constants.ts"
+import { BRAND_ICONS } from "@/features/home-main"
+
+import { checkIfMediaIsInFavorites } from "@/helpers/checkIfMediaIsInFavorites.ts"
+import { formatResponse } from "@/helpers/formatResponse.ts"
+import { updateLocalStorageUserFavorites } from "@/helpers/updateLocalStorageUserFavorites.ts"
+
+import { useAuth } from "@/hooks/auth/useAuth.ts"
+import { useLoadingBackdrop } from "@/hooks/back-drop/useLoadingBackdrop.tsx"
+import { useUpsertFavoriteMedias } from "@/hooks/medias/useUpsertFavoriteMedias.ts"
+
+import { MediaInterface } from "@/interfaces/MediaInterface.ts"
 import {
   AccessTimeFilledOutlined,
   CommentOutlined,
+  DateRange,
+  EighteenUpRatingOutlined,
+  FavoriteOutlined,
   FourKOutlined,
   PlayArrowOutlined,
   PublicOutlined
 } from "@mui/icons-material"
-import { Box, Grid, Typography, useTheme } from "@mui/material"
-
-import movies from "@/assets/images/movies.jpg"
-import { CustomButton } from "@/components/buttons/custom-button"
-import { MediaCard } from "@/components/ui/movie-card"
-import { BRAND_ICONS } from "@/features/home-main"
+import { Box, Grid, IconButton, Skeleton, Typography } from "@mui/material"
+import Chip from "@mui/material/Chip"
+import { useSnackbar } from "notistack"
+import { SyntheticEvent } from "react"
 import { useTranslation } from "react-i18next"
 
 interface MediaInfoDisplayProps {
   isLoading: boolean
+  media: MediaInterface
 }
-export const MediaInfoDisplay = () => {
+
+export const MediaInfoDisplay = ({ isLoading, media }: MediaInfoDisplayProps) => {
+  const { upsertUserFavoriteMedias } = useUpsertFavoriteMedias()
+  const { user, setUser } = useAuth()
+  const { enqueueSnackbar } = useSnackbar()
   const { t } = useTranslation(["common"])
-  const theme = useTheme()
+
+  const { showBackdrop, hideBackdrop, LoadingBackdrop } = useLoadingBackdrop()
+
+  const onAddToFavorites = async (event: SyntheticEvent, media: MediaInterface) => {
+    event.preventDefault()
+    try {
+      showBackdrop()
+
+      const { id: userId, favorites = [] } = user || {}
+      const updatedFavorites = checkIfMediaIsInFavorites(favorites as MediaInterface[], media)
+      const response = await upsertUserFavoriteMedias(userId as string, updatedFavorites)
+
+      const { favorites: updatedServerFavorites } = await formatResponse(response)
+      updateLocalStorageUserFavorites(updatedServerFavorites, setUser)
+      enqueueSnackbar(t("success.successMessage"), { variant: "default" })
+    } catch (error) {
+      enqueueSnackbar(t("error.errorMessage"), { variant: "error" })
+    } finally {
+      hideBackdrop()
+    }
+  }
+
   return (
     <Box
       sx={{
         width: "100%",
-        height: "100vh"
+        minHeight: "90vh",
+        height: { mobile: "50vh", tablet: "50vh", laptop: "50vh", desktop: "auto" }
       }}>
-      <img
-        src={movies}
-        alt="Movies"
-        loading="lazy"
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          filter: "blur(1px) brightness(25%)",
-          zIndex: 0
-        }}
-      />
+      {isLoading ? (
+        <Skeleton
+          variant="rectangular"
+          width="100%"
+          height="100%"
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            borderRadius: "1rem",
+            zIndex: 0
+          }}
+        />
+      ) : (
+        <img
+          src={createBackDropBack(media.backdrop_path as string).replace(
+            TmdbImageSizes.POSTER_ORIGINAL,
+            TmdbImageSizes.POSTER_W1280
+          )}
+          alt={media.title}
+          loading="lazy"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            filter: "blur(1px) brightness(25%)",
+            zIndex: 0
+          }}
+        />
+      )}
       <div
         style={{
           position: "absolute",
@@ -63,10 +123,10 @@ export const MediaInfoDisplay = () => {
           zIndex: 1
         }}
       />
-       <div
+      <div
         style={{
           position: "absolute",
-          background: "linear-gradient(to top, rgba(17,17,17,1) 2%, rgba(0,0,0,0) 50%)", 
+          background: "linear-gradient(to top, rgba(17,17,17,1) 10%, rgba(0,0,0,0) 50%)",
           top: 0,
           left: 0,
           width: "100%",
@@ -86,25 +146,71 @@ export const MediaInfoDisplay = () => {
           <Box
             sx={{
               width: "50%",
+              height: "20rem",
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-between",
               gap: 3,
               zIndex: 100
             }}>
-            <Typography
-              variant="h1"
-              sx={{
-                fontWeight: "900",
-                fontSize: "6rem",
-                letterSpacing: "normal",
-                color: theme.palette.text.primary
-              }}>
-              DEAD POOL
-            </Typography>
+            {isLoading ? (
+              <Skeleton
+                variant="text"
+                width="50%"
+                height={50}
+                sx={{
+                  borderRadius: "1rem"
+                }}
+              />
+            ) : (
+              <Box
+                sx={{
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  gap: 2
+                }}>
+                <Typography
+                  variant="h1"
+                  sx={{
+                    fontWeight: "900",
+                    fontSize: "3rem",
+                    letterSpacing: "normal",
+                    color: "white"
+                  }}>
+                  {media?.original_title}
+                </Typography>
+              </Box>
+            )}
+            <Box sx={{ flex: 1 }}>
+              {isLoading ? (
+                <Skeleton
+                  variant="rectangular"
+                  width="100%"
+                  height={20}
+                  sx={{
+                    borderRadius: "1rem"
+                  }}
+                />
+              ) : (
+                <Box
+                  sx={{
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    alignContent: "center"
+                  }}>
+                  <Typography>{media?.tagline}</Typography>
+                </Box>
+              )}
+            </Box>
             <Box
               sx={{
-                width: "100%",
+                width: "60rem",
                 height: "100%",
                 flex: 1,
                 display: "flex",
@@ -114,88 +220,151 @@ export const MediaInfoDisplay = () => {
                 alignContent: "flex-end",
                 gap: 2
               }}>
-              <Typography variant="body2" sx={{ color: theme.palette.text.disabled }}>
-                Action
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "flex-end",
-                  justifyContent: "flex-start",
-                  alignContent: "flex-start"
-                }}>
-                <Typography variant="body2" sx={{ color: theme.palette.text.disabled }}>
-                  4.5
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "flex-end",
-                  justifyContent: "flex-start",
-                  alignContent: "flex-start"
-                }}>
-                <FourKOutlined fontSize={"small"} sx={{ color: theme.palette.text.disabled }} />
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "flex-end",
-                  justifyContent: "flex-start",
-                  alignContent: "flex-start",
-                  gap: 0.5
-                }}>
-                <AccessTimeFilledOutlined
-                  fontSize={"small"}
-                  sx={{ color: theme.palette.text.disabled }}
-                />
-                <Typography variant="body2" sx={{ color: theme.palette.text.disabled }}>
-                  120
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "flex-end",
-                  justifyContent: "flex-start",
-                  alignContent: "flex-start",
-                  gap: 0.5
-                }}>
-                <PublicOutlined fontSize={"small"} sx={{ color: theme.palette.text.disabled }} />
-                <Typography variant="body2" sx={{ color: theme.palette.text.disabled }}>
-                  English
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  width: "7rem",
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "flex-end",
-                  justifyContent: "flex-start",
-                  alignContent: "flex-start",
-                  gap: 0.5
-                }}>
-                <CommentOutlined fontSize={"small"} sx={{ color: theme.palette.text.disabled }} />
-                <Typography variant="body2" sx={{ color: theme.palette.text.disabled }}>
-                  Subtitles
-                </Typography>
-              </Box>
+              {isLoading ? (
+                <Skeleton variant="text" width="30%" />
+              ) : (
+                <>
+                  <Typography variant="body2" sx={{ color: "gray" }}>
+                    {media.genres?.split(",")[0]}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "gray", display: "flex", gap: 1 }}>
+                    <EighteenUpRatingOutlined fontSize={"small"} sx={{ color: "gray" }} />
+                    {media.vote_average?.toPrecision(2)}
+                  </Typography>
+                  <FourKOutlined fontSize={"small"} sx={{ color: "gray" }} />
+
+                  <Typography variant="body2" sx={{ color: "gray", display: "flex", gap: 1 }}>
+                    <AccessTimeFilledOutlined fontSize={"small"} sx={{ color: "gray" }} />
+                    {media.runtime}m
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "gray", display: "flex", gap: 1 }}>
+                    <PublicOutlined fontSize={"small"} sx={{ color: "gray" }} />
+                    {media.original_language?.toUpperCase()}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "gray", display: "flex", gap: 1 }}>
+                    <CommentOutlined fontSize={"small"} sx={{ color: "gray" }} />
+                    {media.spoken_languages?.split(", ").slice(0, 2).join(", ")}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "gray", display: "flex", gap: 1 }}>
+                    <DateRange fontSize={"small"} sx={{ color: "gray" }} />
+                    {media.release_date?.split("-")[0]}
+                  </Typography>
+                </>
+              )}
             </Box>
-            <Box sx={{ flex: 1 }}>
-              <Typography>{t("dummyText")}</Typography>
+            <Box
+              sx={{
+                width: "60rem",
+                height: "100%",
+                flex: 1,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                textAlign: "flex-end",
+                alignContent: "flex-end",
+                gap: 2
+              }}>
+              {isLoading ? (
+                <Skeleton variant="text" width="30%" />
+              ) : (
+                <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
+                  {media?.production_companies
+                    ?.split(",")
+                    .slice(0, 3)
+                    .map((company) => (
+                      <Chip key={company} label={company} variant={"outlined"} size={"small"} />
+                    ))}
+                </Box>
+              )}
+            </Box>
+            <Box
+              sx={{
+                width: "60rem",
+                height: "100%",
+                flex: 1,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                textAlign: "flex-end",
+                alignContent: "flex-end",
+                gap: 2
+              }}>
+              {isLoading ? (
+                <Skeleton variant="text" width="30%" />
+              ) : (
+                <Grid sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
+                  {media?.production_countries
+                    ?.split(",")
+                    .slice(0, 2)
+                    .map((country) => (
+                      <Chip key={country} label={country} variant={"outlined"} size={"small"} />
+                    ))}
+                </Grid>
+              )}
+            </Box>
+            <Box sx={{ height: "auto" }}>
+              {isLoading ? (
+                <Skeleton
+                  variant="rectangular"
+                  width="100%"
+                  height={100}
+                  sx={{
+                    borderRadius: "1rem"
+                  }}
+                />
+              ) : (
+                <Typography variant={"body2"}>
+                  {media?.overview?.split(" ").slice(1, 70).join(" ")}
+                </Typography>
+              )}
             </Box>
             <Box>
-              <CustomButton variant={ButtonVariant.OUTLINED}>
-                <PlayArrowOutlined />
-                {t("checkMovie")}
-              </CustomButton>
+              {isLoading ? (
+                <Skeleton
+                  variant="rectangular"
+                  width={150}
+                  height={50}
+                  sx={{
+                    borderRadius: "1rem"
+                  }}
+                />
+              ) : (
+                <Box
+                  sx={{
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "flex-start",
+                    gap: 3
+                  }}>
+                  <Box>
+                    <CustomButton variant="outlined" href={media.homepage as string}>
+                      <PlayArrowOutlined />
+                      {t("checkMovie")}
+                    </CustomButton>
+                  </Box>
+                  <Box>
+                    {user?.isLoggedIn && (
+                      <IconButton
+                        aria-label="favorite"
+                        onClick={(event) => onAddToFavorites(event, media)}
+                        sx={{
+                          backgroundColor: "rgba(125, 125, 125, 0.2)"
+                        }}>
+                        <FavoriteOutlined
+                          color={
+                            user?.favorites?.some((fav) => fav.id === media.id)
+                              ? MainColor.WARNING
+                              : MainColor.PRIMARY
+                          }
+                        />
+                      </IconButton>
+                    )}
+                  </Box>
+                </Box>
+              )}
             </Box>
-
             <Box
               sx={{
                 width: "100%",
@@ -206,35 +375,49 @@ export const MediaInfoDisplay = () => {
                 gap: 2
               }}>
               <Grid>
-                <Typography
-                  color={MainColor.PRIMARY}
-                  variant="body1"
-                  sx={{
-                    fontWeight: "bold"
-                  }}>
-                  {t("streamsAt")}
-                </Typography>
-              </Grid>
-              <Grid
-                container
-                sx={{
-                  flex: 1,
-                  gap: 1
-                }}>
-                {BRAND_ICONS.map((brandIcon) => (
-                  <MediaCard
-                    sx={{ width: "4rem", height: "4rem" }}
-                    key={brandIcon}
-                    imageUrl={brandIcon as string}
-                    title={brandIcon as string}
-                    id={brandIcon as string}
+                {isLoading ? (
+                  <Skeleton
+                    variant="rectangular"
+                    height={20}
+                    width={100}
+                    sx={{
+                      borderRadius: "1rem"
+                    }}
                   />
-                ))}
+                ) : (
+                  <Typography color="primary" variant="body1" sx={{ fontWeight: "bold" }}>
+                    Streams on
+                  </Typography>
+                )}
+              </Grid>
+              <Grid container sx={{ flex: 1, gap: 1 }}>
+                {isLoading
+                  ? Array.from(new Array(6)).map((_, index) => (
+                      <Skeleton
+                        key={index}
+                        variant="rectangular"
+                        width={50}
+                        height={50}
+                        sx={{
+                          borderRadius: "1rem"
+                        }}
+                      />
+                    ))
+                  : BRAND_ICONS.map((brandIcon) => (
+                      <MediaCard
+                        sx={{ width: "50px", height: "50px" }}
+                        key={brandIcon}
+                        imageUrl={brandIcon}
+                        title={brandIcon}
+                        id={brandIcon}
+                      />
+                    ))}
               </Grid>
             </Box>
           </Box>
         </Grid>
       </Grid>
+      <LoadingBackdrop />
     </Box>
   )
 }
