854dc5499080ce8f2f26bf6f603feeae7b92541c
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-15 21:41:10 +0200

    Add new icons #5

--- src/features/home-main/media/icons.ts
+++ src/features/home-main/media/icons.ts
@@ -1,10 +1,8 @@
-import icon_1 from "@/assets/icons/icon_1.svg"
-import icon_2 from "@/assets/icons/icon_2.svg"
-import icon_3 from "@/assets/icons/icon_3.svg"
-import icon_4 from "@/assets/icons/icon_4.svg"
-import icon_5 from "@/assets/icons/icon_5.svg"
-import icon_6 from "@/assets/icons/icon_6.svg"
-import icon_7 from "@/assets/icons/icon_7.svg"
-import icon_8 from "@/assets/icons/icon_8.svg"
+import icon_4 from "@/assets/icons/brands/AppleTv.png"
+import icon_3 from "@/assets/icons/brands/Disney.png"
+import icon_2 from "@/assets/icons/brands/HBO.png"
+import icon_1 from "@/assets/icons/brands/Netflix.png"
+import icon_6 from "@/assets/icons/brands/Prime.png"
+import icon_5 from "@/assets/icons/brands/WOW.png"
 
-export const BRAND_ICONS = [icon_2, icon_8, icon_3, icon_4, icon_5, icon_6, icon_7 , icon_1]
+export const BRAND_ICONS = [icon_1, icon_2, icon_3, icon_4, icon_5, icon_6]
