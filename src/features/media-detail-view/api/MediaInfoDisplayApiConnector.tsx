b597733174b8b281eab76d3fc7f0ff2a4fce52b1
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-15 21:44:42 +0200

    Add media  #5

--- src/features/media-detail-view/api/MediaInfoDisplayApiConnector.tsx
+++ src/features/media-detail-view/api/MediaInfoDisplayApiConnector.tsx
@@ -1,7 +1,10 @@
-import { MediaInfoDisplay } from '../components/MediaInfoDisplay'
+import { useFindMediaByImdbId } from "@/hooks/medias/useFindMediaByImdbId.ts"
+import { useParams } from "react-router-dom"
+import { MediaInfoDisplay } from "../components/MediaInfoDisplay"
 
 export const MediaInfoDisplayApiConnector = () => {
-  return (
-    <MediaInfoDisplay/>
-  )
+  const { imdbId } = useParams()
+  const { isLoading, response } = useFindMediaByImdbId({ imdbId: imdbId as string })
+
+  return <MediaInfoDisplay media={response && response[0]} isLoading={isLoading} />
 }
