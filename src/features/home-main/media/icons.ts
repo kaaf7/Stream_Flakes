949c016588675cae7c8f91e9ca7e665b8cde9dfb
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-16 20:23:27 +0200

    Edit icon naming #5

--- src/features/home-main/media/icons.ts
+++ src/features/home-main/media/icons.ts
@@ -1,8 +1,8 @@
-import icon_4 from "@/assets/icons/brands/AppleTv.png"
-import icon_3 from "@/assets/icons/brands/Disney.png"
-import icon_2 from "@/assets/icons/brands/HBO.png"
-import icon_1 from "@/assets/icons/brands/Netflix.png"
-import icon_6 from "@/assets/icons/brands/Prime.png"
-import icon_5 from "@/assets/icons/brands/WOW.png"
+import icon_4 from "@/assets/icons/brands/appletv.png"
+import icon_3 from "@/assets/icons/brands/disney.png"
+import icon_2 from "@/assets/icons/brands/hbo.png"
+import icon_1 from "@/assets/icons/brands/netflix.png"
+import icon_6 from "@/assets/icons/brands/prime.png"
+import icon_5 from "@/assets/icons/brands/wow.png"
 
 export const BRAND_ICONS = [icon_1, icon_2, icon_3, icon_4, icon_5, icon_6]
