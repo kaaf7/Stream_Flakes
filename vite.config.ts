fc97f7bf10da72509e4a4d54256b7eedcfc1b291
Karim <108280260+kaaf7@users.noreply.github.com>
2024-09-17 21:16:14 +0200

    install and configure dynamic import by vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
+import dynamicImport from "vite-plugin-dynamic-import"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dynamicImport()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
